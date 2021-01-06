483f776e23a20738b1b195c58138dfed0d7bc7d4
Nick Barry <itsnickbarry@protonmail.ch>
2021-01-06 14:10:00 -0600

    fix use of incremental nonce for delegatecall internal to test

--- test/multisig/ECDSAMultisigWallet.behavior.js
+++ test/multisig/ECDSAMultisigWallet.behavior.js
@@ -478,7 +478,7 @@ const describeBehaviorOfECDSAMultisigWallet = function ({ deploy, getSigners, ge
           let value = ethers.constants.Zero;
 
           let nonce = nextNonce();
-          let { data } = await instance.populateTransaction.isInvalidNonce(signers[0].address, nextNonce());
+          let { data } = await instance.populateTransaction.isInvalidNonce(signers[0].address, nonce);
 
           let signatures = [];
 
