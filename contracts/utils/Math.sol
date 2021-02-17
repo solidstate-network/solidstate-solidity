6e739f19e2c86f31dbd7905fa3179e2b3cec4ff1
Nick Barry <itsnickbarry@protonmail.ch>
2021-02-16 22:30:43 -0600

    add sqrt method to Math library

--- contracts/utils/Math.sol
+++ contracts/utils/Math.sol
@@ -15,4 +15,21 @@ library Math {
   ) internal pure returns (uint) {
     return (a / 2) + (b / 2) + ((a % 2 + b % 2) / 2);
   }
+
+  /**
+   * @notice estimate square root of number
+   * @dev uses Babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
+   * @param x input number
+   * @return square root
+   */
+  function sqrt (
+    uint x
+  ) internal pure returns (uint y) {
+    uint z = (x + 1) / 2;
+    y = x
+    while (z < y) {
+      y = z;
+      z = (x / z + z) / 2;
+    }
+  }
 }
