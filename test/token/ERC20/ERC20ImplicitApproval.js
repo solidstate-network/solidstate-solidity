440cfe07545420cf8bc6f4d19d19c1c077c9a706
Nick Barry <itsnickbarry@protonmail.ch>
2021-02-18 18:25:14 -0600

    add ERC20ImplicitApproval

---
+++ test/token/ERC20/ERC20ImplicitApproval.js
@@ -0,0 +1,49 @@
+const { expect } = require('chai');
+
+const describeBehaviorOfERC20ImplicitApproval = require('./ERC20ImplicitApproval.behavior.js');
+
+const getImplicitlyApprovedSpender = async function () {
+  const [signer] = await ethers.getSigners();
+  return signer;
+};
+
+const deploy = async function () {
+  const factory = await ethers.getContractFactory('ERC20ImplicitApprovalMock');
+  const instance = await factory.deploy(
+    [(await getImplicitlyApprovedSpender()).address]
+  );
+  return await instance.deployed();
+};
+
+describe('ERC20ImplicitApproval', function () {
+  let instance;
+
+  beforeEach(async function () {
+    instance = await deploy();
+  });
+
+  // eslint-disable-next-line mocha/no-setup-in-describe
+  describeBehaviorOfERC20ImplicitApproval({
+    deploy: () => instance,
+    supply: 0,
+    getImplicitlyApprovedSpender,
+  });
+
+  describe('__internal', function () {
+    describe('#_isImplicitlyApproved', function () {
+      it('returns implicit approval status of address', async function () {
+        expect(
+          await instance.callStatic['isImplicitlyApproved(address)'](
+            ethers.constants.AddressZero
+          )
+        ).to.be.false;
+
+        expect(
+          await instance.callStatic['isImplicitlyApproved(address)'](
+            (await getImplicitlyApprovedSpender()).address
+          )
+        ).to.be.true;
+      });
+    });
+  });
+});
