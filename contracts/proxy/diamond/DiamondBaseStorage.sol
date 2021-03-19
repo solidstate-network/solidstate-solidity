e0e748e6d7e45164d34528b9cc7a5c74d57a9fff
Nick Barry <itsnickbarry@protonmail.ch>
2021-03-18 23:42:52 -0600

    remove unchecked block from addFacetSelectors

--- contracts/proxy/diamond/DiamondBaseStorage.sol
+++ contracts/proxy/diamond/DiamondBaseStorage.sol
@@ -104,41 +104,39 @@ library DiamondBaseStorage {
     bytes32 selectorSlot,
     IDiamondCuttable.FacetCut memory facetCut
   ) internal returns (uint256, bytes32) {
-    unchecked {
+    require(
+      facetCut.target.isContract(),
+      'DiamondBase: added facet has not code'
+    );
+
+    for (uint256 i; i < facetCut.selectors.length; i++) {
+      bytes4 selector = facetCut.selectors[i];
+      bytes32 oldFacet = l.facets[selector];
+
       require(
-        facetCut.target.isContract(),
-        'DiamondBase: added facet has not code'
+        address(bytes20(oldFacet)) == address(0),
+        'DiamondBase: cannot add function that already exists'
       );
 
-      for (uint256 i; i < facetCut.selectors.length; i++) {
-        bytes4 selector = facetCut.selectors[i];
-        bytes32 oldFacet = l.facets[selector];
+      // add facet for selector
+      l.facets[selector] = bytes20(facetCut.target) | bytes32(selectorCount);
+      uint256 selectorInSlotPosition = (selectorCount & 7) << 5;
 
-        require(
-          address(bytes20(oldFacet)) == address(0),
-          'DiamondBase: cannot add function that already exists'
-        );
+      // clear selector position in slot and add selector
+      selectorSlot = (
+        selectorSlot & ~(CLEAR_SELECTOR_MASK >> selectorInSlotPosition)
+      ) | (bytes32(selector) >> selectorInSlotPosition);
 
-        // add facet for selector
-        l.facets[selector] = bytes20(facetCut.target) | bytes32(selectorCount);
-        uint256 selectorInSlotPosition = (selectorCount & 7) << 5;
-
-        // clear selector position in slot and add selector
-        selectorSlot = (
-          selectorSlot & ~(CLEAR_SELECTOR_MASK >> selectorInSlotPosition)
-        ) | (bytes32(selector) >> selectorInSlotPosition);
-
-        // if slot is full then write it to storage
-        if (selectorInSlotPosition == 224) {
-          l.selectorSlots[selectorCount >> 3] = selectorSlot;
-          selectorSlot = 0;
-        }
-
-        selectorCount++;
+      // if slot is full then write it to storage
+      if (selectorInSlotPosition == 224) {
+        l.selectorSlots[selectorCount >> 3] = selectorSlot;
+        selectorSlot = 0;
       }
 
-      return (selectorCount, selectorSlot);
+      selectorCount++;
     }
+
+    return (selectorCount, selectorSlot);
   }
 
   function replaceFacetSelectors (
