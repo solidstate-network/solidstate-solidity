a57b40c75149ebe93bba3368056fa2858cbe2440
Zimri Leijen <zimmah@gmail.com>
2021-04-21 16:55:52 +0200

    add placeholder tests

--- test/token/ERC1155/ERC1155Enumerable.behavior.js
+++ test/token/ERC1155/ERC1155Enumerable.behavior.js
@@ -4,14 +4,14 @@ const describeBehaviorOfERC1155Base = require('./ERC1155Base.behavior.js');
 
 const { describeFilter } = require('@solidstate/library/mocha_describe_filter.js');
 
-const describeBehaviorOfERC20Metadata = function ({ deploy }, skips) {
+const describeBehaviorOfERC1155Enumerable = function ({ deploy }, skips) {
   const describe = describeFilter(skips);
 
   describe('::ERC1155Enumerable', function () {
     let instance;
 
     beforeEach(async function () {
-      instance = await ethers.getContractAt('ERC20Metadata', (await deploy()).address);
+      instance = await ethers.getContractAt('ERC1155Enumerable', (await deploy()).address);
     });
 
     // eslint-disable-next-line mocha/no-setup-in-describe
@@ -20,22 +20,34 @@ const describeBehaviorOfERC20Metadata = function ({ deploy }, skips) {
     }, skips);
 
     describe('#totalSupply', function () {
-      it('returns supply of given token');
+      it.only('returns supply of given token', async function(){
+        let id = ethers.constants.Zero;
+        expect(await instance.callStatic.totalSupply(id)).to.equal(0);
+      });
     });
 
     describe('#totalHolders', function () {
-      it('returns number of holders of given token');
+      it.only('returns number of holders of given token', async function(){
+        let id = ethers.constants.Zero;
+        expect(await instance.callStatic.totalHolders(id)).to.equal(0);
+      });
     });
 
     describe('#accountsByToken', function () {
-      it('returns list of addresses holding given token');
+      it.only('returns list of addresses holding given token', async function(){
+        let id = ethers.constants.Zero;
+        expect(await instance.callStatic.accountsByToken(id)).to.eql([]);
+      });
     });
 
     describe('#tokensByAccount', function () {
-      it('returns list of tokens held by given address');
+      it.only('returns list of tokens held by given address', async function(){
+        let [account] = await ethers.getSigners();
+        expect(await instance.callStatic.tokensByAccount(account.address)).to.eql([])
+      });
     });
   });
 };
 
 // eslint-disable-next-line mocha/no-exports
-module.exports = describeBehaviorOfERC20Metadata;
+module.exports = describeBehaviorOfERC1155Enumerable;
