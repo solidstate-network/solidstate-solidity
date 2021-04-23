23469539f754120f03a738e079067108ab1749e3
Nick Barry <itsnickbarry@protonmail.ch>
2021-04-23 15:38:20 -0500

    test ERC1155Enumerable transfers

--- test/token/ERC1155/ERC1155Enumerable.behavior.js
+++ test/token/ERC1155/ERC1155Enumerable.behavior.js
@@ -7,6 +7,9 @@ const { describeFilter } = require('@solidstate/library/mocha_describe_filter.js
 const describeBehaviorOfERC1155Enumerable = function ({ deploy }, skips) {
   const describe = describeFilter(skips);
 
+  // TODO: accept mint function as parameter
+  // TODO: test burns
+
   describe('::ERC1155Enumerable', function () {
     let instance;
 
@@ -21,12 +24,23 @@ const describeBehaviorOfERC1155Enumerable = function ({ deploy }, skips) {
 
     describe('#totalSupply', function () {
       it('returns supply of given token', async function(){
+        const [holder0, holder1] = await ethers.getSigners();
         const id = ethers.constants.Zero;
+        const amount = ethers.constants.Two;
+
         expect(await instance.callStatic.totalSupply(id)).to.equal(0);
 
-        const [signer] = await ethers.getSigners();
-        const amount = ethers.constants.Two;
-        await instance.mint(signer.address, id, amount, ethers.utils.randomBytes(0));
+        await instance.mint(holder0.address, id, amount, ethers.utils.randomBytes(0));
+
+        expect(await instance.callStatic.totalSupply(id)).to.equal(amount);
+
+        await instance.connect(holder0).safeTransferFrom(
+          holder0.address,
+          holder1.address,
+          id,
+          amount,
+          ethers.utils.randomBytes(0)
+        );
 
         expect(await instance.callStatic.totalSupply(id)).to.equal(amount);
       });
@@ -34,12 +48,23 @@ const describeBehaviorOfERC1155Enumerable = function ({ deploy }, skips) {
 
     describe('#totalHolders', function () {
       it('returns number of holders of given token', async function(){
+        const [holder0, holder1] = await ethers.getSigners();
         const id = ethers.constants.Zero;
+        const amount = ethers.constants.Two;
+
         expect(await instance.callStatic.totalHolders(id)).to.equal(0);
 
-        const [signer] = await ethers.getSigners();
-        const amount = ethers.constants.Two;
-        await instance.mint(signer.address, id, amount, ethers.utils.randomBytes(0));
+        await instance.mint(holder0.address, id, amount, ethers.utils.randomBytes(0));
+
+        expect(await instance.callStatic.totalHolders(id)).to.equal(1);
+
+        await instance.connect(holder0).safeTransferFrom(
+          holder0.address,
+          holder1.address,
+          id,
+          amount,
+          ethers.utils.randomBytes(0)
+        );
 
         expect(await instance.callStatic.totalHolders(id)).to.equal(1);
       });
@@ -47,26 +72,52 @@ const describeBehaviorOfERC1155Enumerable = function ({ deploy }, skips) {
 
     describe('#accountsByToken', function () {
       it('returns list of addresses holding given token', async function(){
+        const [holder0, holder1] = await ethers.getSigners();
         const id = ethers.constants.Zero;
+        const amount = ethers.constants.Two;
+
         expect(await instance.callStatic.accountsByToken(id)).to.eql([]);
 
-        const [signer] = await ethers.getSigners();
-        const amount = ethers.constants.Two;
-        await instance.mint(signer.address, id, amount, ethers.utils.randomBytes(0));
+        await instance.mint(holder0.address, id, amount, ethers.utils.randomBytes(0));
 
-        expect(await instance.callStatic.accountsByToken(id)).to.eql([signer.address]);
+        expect(await instance.callStatic.accountsByToken(id)).to.eql([holder0.address]);
+
+        await instance.connect(holder0).safeTransferFrom(
+          holder0.address,
+          holder1.address,
+          id,
+          amount,
+          ethers.utils.randomBytes(0)
+        );
+
+        expect(await instance.callStatic.accountsByToken(id)).to.eql([holder1.address]);
       });
     });
 
     describe('#tokensByAccount', function () {
       it('returns list of tokens held by given address', async function(){
+        const [holder0, holder1] = await ethers.getSigners();
         const id = ethers.constants.Zero;
-        const [signer] = await ethers.getSigners();
-        expect(await instance.callStatic.tokensByAccount(signer.address)).to.eql([])
-
         const amount = ethers.constants.Two;
-        await instance.mint(signer.address, id, amount, ethers.utils.randomBytes(0));
-        expect(await instance.callStatic.tokensByAccount(signer.address)).to.eql([id])
+
+        expect(await instance.callStatic.tokensByAccount(holder0.address)).to.eql([]);
+        expect(await instance.callStatic.tokensByAccount(holder1.address)).to.eql([]);
+
+        await instance.mint(holder0.address, id, amount, ethers.utils.randomBytes(0));
+
+        expect(await instance.callStatic.tokensByAccount(holder0.address)).to.eql([id]);
+        expect(await instance.callStatic.tokensByAccount(holder1.address)).to.eql([]);
+
+        await instance.connect(holder0).safeTransferFrom(
+          holder0.address,
+          holder1.address,
+          id,
+          amount,
+          ethers.utils.randomBytes(0)
+        );
+
+        expect(await instance.callStatic.tokensByAccount(holder0.address)).to.eql([]);
+        expect(await instance.callStatic.tokensByAccount(holder1.address)).to.eql([id]);
       });
     });
   });
