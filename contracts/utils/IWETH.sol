8015e4709c14da3fce7ba117b27ecd53182e9352
Nick Barry <itsnickbarry@protonmail.ch>
2021-05-03 23:36:00 -0500

    add IWETH NatSpec comments

--- contracts/utils/IWETH.sol
+++ contracts/utils/IWETH.sol
@@ -5,9 +5,20 @@ pragma solidity ^0.8.0;
 import '../token/ERC20/IERC20.sol';
 import '../token/ERC20/IERC20Metadata.sol';
 
+/**
+ * @title WETH (Wrapped ETH) interface
+ */
 interface IWETH is IERC20, IERC20Metadata {
+  /**
+   * @notice convert ETH to WETH
+   */
   function deposit () external payable;
 
+  /**
+   * @notice convert WETH to ETH
+   * @dev if caller is a contract, it should have a fallback or receive function
+   * @param amount quantity of WETH to convert, denominated in wei
+   */
   function withdraw (
     uint amount
   ) external;
