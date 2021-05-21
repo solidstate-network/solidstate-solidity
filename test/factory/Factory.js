d0b97573955b8ae2bd3119e5f1c6df4f6b2a7ef6
Nick Barry <itsnickbarry@protonmail.ch>
2021-05-21 17:55:49 -0500

    test factory base deployment code

--- test/factory/Factory.js
+++ test/factory/Factory.js
@@ -2,17 +2,13 @@ const { expect } = require('chai');
 
 const describeBehaviorOfFactory = require('@solidstate/spec/factory/Factory.behavior.js');
 
-let deploy = async function () {
-  let factory = await ethers.getContractFactory('FactoryMock');
-  let instance = await factory.deploy();
-  return await instance.deployed();
-};
-
 describe('Factory', function () {
   let instance;
 
   beforeEach(async function () {
-    instance = await deploy();
+    const factory = await ethers.getContractFactory('FactoryMock');
+    instance = await factory.deploy();
+    await instance.deployed();
   });
 
   // eslint-disable-next-line mocha/no-setup-in-describe
@@ -22,35 +18,43 @@ describe('Factory', function () {
     describe('#_deploy', function () {
       describe('(bytes)', function () {
         it('deploys bytecode and returns deployment address', async function () {
-          let initCode = instance.deployTransaction.data;
+          const initCode = instance.deployTransaction.data;
 
-          let address = await instance.callStatic['deploy(bytes)'](initCode);
+          const address = await instance.callStatic['deploy(bytes)'](initCode);
           expect(address).to.be.properAddress;
 
-          // await instance['deploy(bytes)'](initCode);
-          // let deployed = await ethers.getContractAt('Factory', address);
-          // TODO: assert code at address is correct
+          await instance['deploy(bytes)'](initCode);
+
+          expect(
+            await ethers.provider.getCode(address)
+          ).to.equal(
+            await ethers.provider.getCode(instance.address)
+          );
         });
       });
 
       describe('(bytes,bytes32)', function () {
         it('deploys bytecode and returns deployment address', async function () {
-          let initCode = await instance.deployTransaction.data;
-          let initCodeHash = ethers.utils.keccak256(initCode);
-          let salt = ethers.utils.randomBytes(32);
+          const initCode = await instance.deployTransaction.data;
+          const initCodeHash = ethers.utils.keccak256(initCode);
+          const salt = ethers.utils.randomBytes(32);
 
-          let address = await instance.callStatic['deploy(bytes,bytes32)'](initCode, salt);
+          const address = await instance.callStatic['deploy(bytes,bytes32)'](initCode, salt);
           expect(address).to.equal(await instance.calculateDeploymentAddress(initCodeHash, salt));
 
-          // await instance['deploy(bytes)'](initCode);
-          // let deployed = await ethers.getContractAt('Factory', address);
-          // TODO: assert code at address is correct
+          await instance['deploy(bytes,bytes32)'](initCode, salt);
+
+          expect(
+            await ethers.provider.getCode(address)
+          ).to.equal(
+            await ethers.provider.getCode(instance.address)
+          );
         });
 
         describe('reverts if', function () {
           it('salt has already been used', async function () {
-            let initCode = instance.deployTransaction.data;
-            let salt = ethers.utils.randomBytes(32);
+            const initCode = instance.deployTransaction.data;
+            const salt = ethers.utils.randomBytes(32);
 
             await instance['deploy(bytes,bytes32)'](initCode, salt);
 
@@ -66,9 +70,9 @@ describe('Factory', function () {
 
     describe('#_calculateDeploymentAddress', function () {
       it('returns address of not-yet-deployed contract', async function () {
-        let initCode = instance.deployTransaction.data;
-        let initCodeHash = ethers.utils.keccak256(initCode);
-        let salt = ethers.utils.randomBytes(32);
+        const initCode = instance.deployTransaction.data;
+        const initCodeHash = ethers.utils.keccak256(initCode);
+        const salt = ethers.utils.randomBytes(32);
 
         expect(
           await instance.callStatic.calculateDeploymentAddress(initCodeHash, salt)
