477672d663726fbdef03751702b530165a231816
Nick Barry <itsnickbarry@protonmail.ch>
2021-06-01 17:52:59 -0500

    use ECDSA library for ERC20Permit

--- contracts/token/ERC20/ERC20Permit.sol
+++ contracts/token/ERC20/ERC20Permit.sol
@@ -5,6 +5,7 @@ import "./ERC20Base.sol";
 import "./ERC20PermitStorage.sol";
 import "./IERC2612Permit.sol";
 import "./ERC20Metadata.sol";
+import {ECDSA} from '../../cryptography/ECDSA.sol';
 
 // ERC20-Permit implementation from soliditylabs ( https://github.com/soliditylabs/ERC20-Permit )
 
@@ -17,6 +18,7 @@ import "./ERC20Metadata.sol";
  * The {permit} signature mechanism conforms to the {IERC2612Permit} interface.
  */
 abstract contract ERC20Permit is ERC20Base, ERC20Metadata, IERC2612Permit {
+  using ECDSA for bytes32;
   /**
    * @dev See {IERC2612Permit-permit}.
    *
@@ -84,7 +86,7 @@ abstract contract ERC20Permit is ERC20Base, ERC20Metadata, IERC2612Permit {
       hash := keccak256(memPtr, 66)
     }
 
-    address signer = _recover(hash, v, r, s);
+    address signer = hash.recover(v, r, s);
 
     require(signer == owner, "ERC20Permit: invalid signature");
 
@@ -138,38 +140,5 @@ abstract contract ERC20Permit is ERC20Base, ERC20Metadata, IERC2612Permit {
     }
 
     return chainID;
-  }
-
-  function _recover(
-    bytes32 hash,
-    uint8 v,
-    bytes32 r,
-    bytes32 s
-  ) internal pure returns (address) {
-    // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and make the signature
-    // unique. Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
-    // the valid range for s in (281): 0 < s < secp256k1n ÷ 2 + 1, and for v in (282): v ∈ {27, 28}. Most
-    // signatures from current libraries generate a unique signature with an s-value in the lower half order.
-    //
-    // If your library generates malleable signatures, such as s-values in the upper range, calculate a new s-value
-    // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v from 27 to 28 or
-    // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27 to v to accept
-    // these malleable signatures as well.
-    if (
-      uint256(s) >
-      0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0
-    ) {
-      revert("ECDSA: invalid signature 's' value");
-    }
-
-    if (v != 27 && v != 28) {
-      revert("ECDSA: invalid signature 'v' value");
-    }
-
-    // If the signature is valid (and not malleable), return the signer address
-    address signer = ecrecover(hash, v, r, s);
-    require(signer != address(0), "ECDSA: invalid signature");
-
-    return signer;
   }
 }
