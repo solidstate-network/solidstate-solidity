158b157eaea46979901245a46fa7291a159cd198
Lumyo <lumyo.b@gmail.com>
2021-06-04 08:59:25 +0800

    Add ERC20 burn revert message if burn amount exceeds balance

--- test/token/ERC20/ERC20.ts
+++ test/token/ERC20/ERC20.ts
@@ -1,6 +1,8 @@
+import { expect } from 'chai';
 import { describeBehaviorOfERC20 } from '@solidstate/spec';
 import { ethers } from 'hardhat';
 import { ERC20Mock, ERC20Mock__factory } from '../../../typechain';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
 const name = 'ERC20Metadata.name';
 const symbol = 'ERC20Metadata.symbol';
@@ -9,9 +11,11 @@ const supply = ethers.utils.parseEther('1');
 
 describe('ERC20', function () {
   let instance: ERC20Mock;
+  let deployer: SignerWithAddress;
+  let user: SignerWithAddress;
 
   beforeEach(async function () {
-    const [deployer] = await ethers.getSigners();
+    [deployer, user] = await ethers.getSigners();
     instance = await new ERC20Mock__factory(deployer).deploy(
       name,
       symbol,
@@ -30,5 +34,25 @@ describe('ERC20', function () {
     symbol,
     decimals,
     supply,
+  });
+
+  describe('__internal', () => {
+    describe('#_burn', () => {
+      it('burn tokens', async () => {
+        await instance.mint(user.address, 100);
+        expect(await instance.balanceOf(user.address)).to.eq(100);
+        await instance.burn(user.address, 90);
+        expect(await instance.balanceOf(user.address)).to.eq(10);
+        await instance.burn(user.address, 10);
+        expect(await instance.balanceOf(user.address)).to.eq(0);
+      });
+
+      it('reverts if burn amount exceeds balance', async () => {
+        await instance.mint(user.address, 100);
+        await expect(instance.burn(user.address, 101)).to.be.revertedWith(
+          'ERC20: burn amount exceeds balance',
+        );
+      });
+    });
   });
 });
