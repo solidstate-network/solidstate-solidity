2ad517bce1b1e7350216a1de55d02a6dd252c897
Lumyo <lumyo.b@gmail.com>
2021-06-05 14:14:32 +0800

    Move test to ERC20Base.ts

--- test/token/ERC20/ERC20.ts
+++ test/token/ERC20/ERC20.ts
@@ -11,11 +11,9 @@ const supply = ethers.utils.parseEther('1');
 
 describe('ERC20', function () {
   let instance: ERC20Mock;
-  let deployer: SignerWithAddress;
-  let user: SignerWithAddress;
 
   beforeEach(async function () {
-    [deployer, user] = await ethers.getSigners();
+    const [deployer] = await ethers.getSigners();
     instance = await new ERC20Mock__factory(deployer).deploy(
       name,
       symbol,
@@ -34,25 +32,5 @@ describe('ERC20', function () {
     symbol,
     decimals,
     supply,
-  });
-
-  describe('__internal', () => {
-    describe('#_burn', () => {
-      it('burn tokens', async () => {
-        await instance.mint(user.address, 100);
-        expect(await instance.balanceOf(user.address)).to.eq(100);
-        await instance.burn(user.address, 90);
-        expect(await instance.balanceOf(user.address)).to.eq(10);
-        await instance.burn(user.address, 10);
-        expect(await instance.balanceOf(user.address)).to.eq(0);
-      });
-
-      it('reverts if burn amount exceeds balance', async () => {
-        await instance.mint(user.address, 100);
-        await expect(instance.burn(user.address, 101)).to.be.revertedWith(
-          'ERC20: burn amount exceeds balance',
-        );
-      });
-    });
   });
 });
