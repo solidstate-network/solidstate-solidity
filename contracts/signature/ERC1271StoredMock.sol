f290c1a0a6cf3e9c67d5de881030f8852c56cfc0
Nick Barry <itsnickbarry@protonmail.ch>
2021-06-09 00:36:09 -0500

    remove unused ERC1271StoredMock constructor argument

--- contracts/signature/ERC1271StoredMock.sol
+++ contracts/signature/ERC1271StoredMock.sol
@@ -7,8 +7,7 @@ import {ERC1271Stored} from './ERC1271Stored.sol';
 
 contract ERC1271StoredMock is ERC1271Stored {
   constructor (
-    bytes32 hash,
-    bytes memory
+    bytes32 hash
   ) {
     ERC1271StoredStorage.layout().hashes[hash] = true;
   }
