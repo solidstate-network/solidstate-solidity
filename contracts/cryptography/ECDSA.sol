df7ec6cf232d7ba3f9477e0942f882549a740e90
Nick Barry <itsnickbarry@protonmail.ch>
2021-06-10 20:52:46 -0500

    update ECDSA comments

--- contracts/cryptography/ECDSA.sol
+++ contracts/cryptography/ECDSA.sol
@@ -8,18 +8,10 @@ pragma solidity ^0.8.0;
  */
 library ECDSA {
   /**
-   * @dev Returns the address that signed a hashed message (`hash`) with
-   * `signature`. This address can then be used for verification purposes.
-   *
-   * The `ecrecover` EVM opcode allows for malleable (non-unique) signatures:
-   * this function rejects them by requiring the `s` value to be in the lower
-   * half order, and the `v` value to be either 27 or 28.
-   *
-   * IMPORTANT: `hash` _must_ be the result of a hash operation for the
-   * verification to be secure: it is possible to craft signatures that
-   * recover to arbitrary addresses for non-hashed data. A safe way to ensure
-   * this is by receiving a hash of the original message (which may otherwise
-   * be too long), and then calling {toEthSignedMessageHash} on it.
+   * @notice recover signer of hashed message from signature
+   * @param hash hashed data payload
+   * @param signature signed data payload
+   * @return recovered message signer
    */
   function recover(
     bytes32 hash,
@@ -41,7 +33,12 @@ library ECDSA {
   }
 
   /**
-   * @notice recover signer of TODO
+   * @notice recover signer of hashed message from signature v, r, and s values
+   * @param hash hashed data payload
+   * @param v signature "v" value
+   * @param r signature "r" value
+   * @param s signature "s" value
+   * @return recovered message signer
    */
   function recover (
     bytes32 hash,
@@ -72,12 +69,9 @@ library ECDSA {
   }
 
   /**
-   * @dev Returns an Ethereum Signed Message, created from a `hash`. This
-   * replicates the behavior of the
-   * https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_sign[`eth_sign`]
-   * JSON-RPC method.
-   *
-   * See {recover}.
+   * @notice generate an "Ethereum Signed Message" in the format returned by the eth_sign JSON-RPC method
+   * @param hash hashed data payload
+   * @return signed message
    */
   function toEthSignedMessageHash (
     bytes32 hash
