12f7c1c7d0cf1f12d8dc6f26762a075bb3bbab18
Nick Barry <itsnickbarry@protonmail.ch>
2021-08-18 20:05:52 -0500

    add NatSpec comments to access contracts

--- contracts/access/IERC173.sol
+++ contracts/access/IERC173.sol
@@ -2,9 +2,22 @@
 
 pragma solidity ^0.8.0;
 
+/**
+ * @title Contract ownership standard interface
+ * @dev see https://eips.ethereum.org/EIPS/eip-173
+ */
 interface IERC173 {
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
+  /**
+   * @notice get the ERC173 contract owner
+   * @return conract owner
+   */
   function owner () external view returns (address);
-  function transferOwnership (address newOwner) external;
+
+  /**
+   * @notice transfer contract ownership to new account
+   * @param account address of new owner
+   */
+  function transferOwnership (address account) external;
 }
--- contracts/access/SafeOwnable.sol
+++ contracts/access/SafeOwnable.sol
@@ -6,6 +6,9 @@ import {Ownable, OwnableStorage} from './Ownable.sol';
 import {SafeOwnableInternal} from './SafeOwnableInternal.sol';
 import {SafeOwnableStorage} from './SafeOwnableStorage.sol';
 
+/**
+ * @title Ownership access control based on ERC173 with ownership transfer safety check
+ */
 abstract contract SafeOwnable is Ownable, SafeOwnableInternal {
   using OwnableStorage for OwnableStorage.Layout;
   using SafeOwnableStorage for SafeOwnableStorage.Layout;
@@ -14,12 +17,19 @@ abstract contract SafeOwnable is Ownable, SafeOwnableInternal {
     return SafeOwnableStorage.layout().nomineeOwner;
   }
 
+  /**
+   * @inheritdoc Ownable
+   * @dev ownership transfer must be accepted by beneficiary before transfer is complete
+   */
   function transferOwnership (
     address account
   ) virtual override public onlyOwner {
     SafeOwnableStorage.layout().setNomineeOwner(account);
   }
 
+  /**
+   * @notice accept transfer of contract ownership
+   */
   function acceptOwnership () virtual public onlyNomineeOwner {
     OwnableStorage.Layout storage l = OwnableStorage.layout();
     emit OwnershipTransferred(l.owner, msg.sender);
