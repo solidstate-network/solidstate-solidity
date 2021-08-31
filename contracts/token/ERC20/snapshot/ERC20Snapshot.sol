3c61b1126d23a13e0212b0ea31161ef696b081ef
Nick Barry <itsnickbarry@protonmail.ch>
2021-08-31 17:58:36 -0500

    separate ERC20Snapshot internal functions into new contract

--- contracts/token/ERC20/snapshot/ERC20Snapshot.sol
+++ contracts/token/ERC20/snapshot/ERC20Snapshot.sol
@@ -3,16 +3,13 @@
 pragma solidity ^0.8.0;
 
 import {Array} from '../../../utils/Array.sol';
-import {ERC20Base, ERC20BaseInternal} from '../base/ERC20Base.sol';
-import {ERC20SnapshotStorage} from './ERC20SnapshotStorage.sol';
+import {ERC20SnapshotInternal, ERC20SnapshotStorage} from './ERC20SnapshotInternal.sol';
 
 /**
  * @title ERC20 base implementation with support for token balance and supply snapshots
  */
-abstract contract ERC20Snapshot is ERC20Base {
+abstract contract ERC20Snapshot is ERC20SnapshotInternal {
   using Array for uint[];
-
-  event Snapshot (uint id);
 
   /**
    * @notice query the token balance of given account at given snapshot id
@@ -28,7 +25,7 @@ abstract contract ERC20Snapshot is ERC20Base {
       snapshotId,
       ERC20SnapshotStorage.layout().accountBalanceSnapshots[account]
     );
-    return snapshotted ? value : balanceOf(account);
+    return snapshotted ? value : _balanceOf(account);
   }
 
   /**
@@ -43,17 +40,7 @@ abstract contract ERC20Snapshot is ERC20Base {
       snapshotId,
       ERC20SnapshotStorage.layout().totalSupplySnapshots
     );
-    return snapshotted ? value : totalSupply();
-  }
-
-  function _snapshot () virtual internal returns (uint) {
-    ERC20SnapshotStorage.Layout storage l = ERC20SnapshotStorage.layout();
-
-    l.snapshotId++;
-
-    uint current = l.snapshotId;
-    emit Snapshot(current);
-    return current;
+    return snapshotted ? value : _totalSupply();
   }
 
   function _valueAt (
@@ -74,63 +61,6 @@ abstract contract ERC20Snapshot is ERC20Base {
       return (false, 0);
     } else {
       return (true, snapshots.values[index]);
-    }
-  }
-
-  function _updateAccountSnapshot (
-    address account
-  ) private {
-    _updateSnapshot(
-      ERC20SnapshotStorage.layout().accountBalanceSnapshots[account],
-      balanceOf(account)
-    );
-  }
-
-  function _updateTotalSupplySnapshot () private {
-    _updateSnapshot(
-      ERC20SnapshotStorage.layout().totalSupplySnapshots,
-      totalSupply()
-    );
-  }
-
-  function _updateSnapshot (
-    ERC20SnapshotStorage.Snapshots storage snapshots,
-    uint value
-  ) private {
-    uint current = ERC20SnapshotStorage.layout().snapshotId;
-
-    if (_lastSnapshotId(snapshots.ids) < current) {
-      snapshots.ids.push(current);
-      snapshots.values.push(value);
-    }
-  }
-
-  function _lastSnapshotId (
-    uint[] storage ids
-  ) private view returns (uint) {
-    return ids.length == 0 ? 0 : ids[ids.length - 1];
-  }
-
-  /**
-   * @notice ERC20 hook: update snapshot data
-   * @inheritdoc ERC20BaseInternal
-   */
-  function _beforeTokenTransfer (
-    address from,
-    address to,
-    uint amount
-  ) virtual override internal {
-    super._beforeTokenTransfer(from, to, amount);
-
-    if (from == address(0)) {
-      _updateAccountSnapshot(to);
-      _updateTotalSupplySnapshot();
-    } else if (to == address(0)) {
-      _updateAccountSnapshot(from);
-      _updateTotalSupplySnapshot();
-    } else {
-      _updateAccountSnapshot(to);
-      _updateAccountSnapshot(from);
     }
   }
 }
---
+++ contracts/token/ERC20/snapshot/ERC20SnapshotInternal.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// TODO: remove ERC20BaseStorage import
+import {ERC20BaseInternal, ERC20BaseStorage} from '../base/ERC20Base.sol';
+import {ERC20SnapshotStorage} from './ERC20SnapshotStorage.sol';
+
+/**
+ * @title ERC20Snapshot internal functions
+ */
+abstract contract ERC20SnapshotInternal is ERC20BaseInternal {
+  event Snapshot (uint id);
+
+  // TODO: define _balanceOf and _totalSupply functions in ERC20BaseInternal
+
+  function _balanceOf (
+    address account
+  ) internal view returns (uint) {
+    return ERC20BaseStorage.layout().balances[account];
+  }
+
+  function _totalSupply () internal view returns (uint) {
+    return ERC20BaseStorage.layout().totalSupply;
+  }
+
+  function _snapshot () virtual internal returns (uint) {
+    ERC20SnapshotStorage.Layout storage l = ERC20SnapshotStorage.layout();
+
+    l.snapshotId++;
+
+    uint current = l.snapshotId;
+    emit Snapshot(current);
+    return current;
+  }
+
+  function _updateAccountSnapshot (
+    address account
+  ) private {
+    _updateSnapshot(
+      ERC20SnapshotStorage.layout().accountBalanceSnapshots[account],
+      _balanceOf(account)
+    );
+  }
+
+  function _updateTotalSupplySnapshot () private {
+    _updateSnapshot(
+      ERC20SnapshotStorage.layout().totalSupplySnapshots,
+      _totalSupply()
+    );
+  }
+
+  function _updateSnapshot (
+    ERC20SnapshotStorage.Snapshots storage snapshots,
+    uint value
+  ) private {
+    uint current = ERC20SnapshotStorage.layout().snapshotId;
+
+    if (_lastSnapshotId(snapshots.ids) < current) {
+      snapshots.ids.push(current);
+      snapshots.values.push(value);
+    }
+  }
+
+  function _lastSnapshotId (
+    uint[] storage ids
+  ) private view returns (uint) {
+    return ids.length == 0 ? 0 : ids[ids.length - 1];
+  }
+
+  /**
+   * @notice ERC20 hook: update snapshot data
+   * @inheritdoc ERC20BaseInternal
+   */
+  function _beforeTokenTransfer (
+    address from,
+    address to,
+    uint amount
+  ) virtual override internal {
+    super._beforeTokenTransfer(from, to, amount);
+
+    if (from == address(0)) {
+      _updateAccountSnapshot(to);
+      _updateTotalSupplySnapshot();
+    } else if (to == address(0)) {
+      _updateAccountSnapshot(from);
+      _updateTotalSupplySnapshot();
+    } else {
+      _updateAccountSnapshot(to);
+      _updateAccountSnapshot(from);
+    }
+  }
+}
