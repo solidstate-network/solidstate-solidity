fb44061a6e1875e26d3b475c85bc8099fcd3b4dd
Nick Barry <itsnickbarry@protonmail.ch>
2021-09-01 17:48:40 -0500

    separate ERC1271Stored internal functions into new contract

--- contracts/signature/stored/ERC1271Stored.sol
+++ contracts/signature/stored/ERC1271Stored.sol
@@ -3,23 +3,9 @@
 pragma solidity ^0.8.0;
 
 import {ERC1271Base} from '../base/ERC1271Base.sol';
-import {ERC1271StoredStorage} from './ERC1271StoredStorage.sol';
+import {ERC1271StoredInternal, ERC1271StoredStorage} from './ERC1271StoredInternal.sol';
 
 /**
  * @title ERC1271 implementation which validates signatures against internal storage
  */
-abstract contract ERC1271Stored is ERC1271Base {
-  function _isValidSignature (
-    bytes32 hash,
-    bytes memory
-  ) virtual override internal view returns (bool) {
-    return ERC1271StoredStorage.layout().hashes[hash];
-  }
-
-  function _setValidSignature (
-    bytes32 hash,
-    bool status
-  ) internal {
-    ERC1271StoredStorage.layout().hashes[hash] = status;
-  }
-}
+abstract contract ERC1271Stored is ERC1271StoredInternal, ERC1271Base {}
