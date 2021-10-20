5919aad51c27bcb3dc7260b387d90508be38c946
Nick Barry <itsnickbarry@protonmail.ch>
2021-10-20 17:51:31 -0500

    add placeholder ERC1155Metadata tests

---
+++ test/token/ERC1155/ERC1155Metadata.ts
@@ -0,0 +1,23 @@
+import { ethers } from 'hardhat';
+import { describeBehaviorOfERC1155Metadata } from '@solidstate/spec';
+import {
+  ERC1155MetadataMock,
+  ERC1155MetadataMock__factory,
+} from '../../../typechain';
+
+describe('ERC1155Metadata', function () {
+  const tokenURI = 'ERC1155Metadata.tokenURI';
+  let instance: ERC1155MetadataMock;
+
+  beforeEach(async function () {
+    const [deployer] = await ethers.getSigners();
+    instance = await new ERC1155MetadataMock__factory(deployer).deploy(
+      tokenURI,
+    );
+  });
+
+  describeBehaviorOfERC1155Metadata({
+    deploy: async () => instance,
+    tokenURI,
+  });
+});
