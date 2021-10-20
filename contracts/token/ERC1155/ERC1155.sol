d1e532a45a58f8c7536b8bf56461bdf20eecd23a
Nick Barry <itsnickbarry@protonmail.ch>
2021-10-20 17:42:14 -0500

    include ERC1155Metadata extension in ERC1155 contract

--- contracts/token/ERC1155/ERC1155.sol
+++ contracts/token/ERC1155/ERC1155.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.0;
 
 import { ERC165 } from '../../introspection/ERC165.sol';
 import { ERC1155Base } from './base/ERC1155Base.sol';
+import { ERC1155Metadata } from './metadata/ERC1155Metadata.sol';
 
 /**
  * @title SolidState ERC1155 implementation
  */
-abstract contract ERC1155 is ERC1155Base, ERC165 {
+abstract contract ERC1155 is ERC1155Base, ERC1155Metadata, ERC165 {
 
 }
