f5cb17bf76776b42eb2b708719067828c9e7667d
Nick Barry <itsnickbarry@protonmail.ch>
2021-11-04 16:07:53 -0600

    fix DiamondCuttable error message

--- spec/proxy/diamond/DiamondCuttable.behavior.ts
+++ spec/proxy/diamond/DiamondCuttable.behavior.ts
@@ -425,9 +425,7 @@ export function describeBehaviorOfDiamondCuttable(
               ethers.constants.AddressZero,
               '0x',
             ),
-          ).to.be.revertedWith(
-            "Hardhat couldn't infer the reason. Please report this to help us improve Hardhat.",
-          );
+          ).to.be.revertedWith('Transaction reverted without a reason string');
         });
 
         it('passed selector array is empty', async function () {
