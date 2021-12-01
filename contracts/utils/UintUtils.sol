775cf3b807bca1514750f73a928a62bba2263d92
Nick Barry <itsnickbarry@protonmail.ch>
2021-11-30 21:24:34 -0600

    add toHexString functions and NatSpec description to UintUtils

--- contracts/utils/UintUtils.sol
+++ contracts/utils/UintUtils.sol
@@ -2,7 +2,13 @@
 
 pragma solidity ^0.8.0;
 
+/**
+ * @title utility functions for uint256 operations
+ * @dev derived from https://github.com/OpenZeppelin/openzeppelin-contracts/ (MIT license)
+ */
 library UintUtils {
+    bytes16 private constant HEX_SYMBOLS = '0123456789abcdef';
+
     function toString(uint256 value) internal pure returns (string memory) {
         if (value == 0) {
             return '0';
@@ -23,6 +29,41 @@ library UintUtils {
             buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
             value /= 10;
         }
+
+        return string(buffer);
+    }
+
+    function toHexString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return '0x00';
+        }
+
+        uint256 temp = value;
+        uint256 length = 0;
+
+        while (temp != 0) {
+            length++;
+            temp >>= 8;
+        }
+
+        return toHexString(value, length);
+    }
+
+    function toHexString(uint256 value, uint256 length)
+        internal
+        pure
+        returns (string memory)
+    {
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = '0';
+        buffer[1] = 'x';
+
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = HEX_SYMBOLS[value & 0xf];
+            value >>= 4;
+        }
+
+        require(value == 0, 'Strings: hex length insufficient');
 
         return string(buffer);
     }
