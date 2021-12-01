7ff7e5f36b237f08daa555fa72efa9aff020c879
NouDaimon <peric.iason@gmail.com>
2021-12-01 12:31:57 +0200

    tests suite UintUtils

--- test/utils/UintUtils.ts
+++ test/utils/UintUtils.ts
@@ -31,11 +31,74 @@ describe('UintUtils', function () {
 
     describe('#toHexString', function () {
       describe('(uint256)', function () {
-        it('todo');
+        it('Should return 0 if input is 0', async () => {
+          const zeroNumber = ethers.BigNumber.from('0');
+          expect(
+            await instance.callStatic['toHexString(uint256)'](zeroNumber),
+          ).to.equal('0x00');
+        });
+
+        it('Should return correct hexString represenation of a number', async () => {
+          const inputValues = [
+            '1000',
+            '1',
+            '12345',
+            '85746201361230',
+            '999983',
+          ];
+          const outputValues = [
+            '0x03e8',
+            '0x01',
+            '0x3039',
+            '0x4dfc57df7b4e',
+            '0x0f422f',
+          ];
+          for (let i = 0; i < inputValues.length; i++) {
+            expect(
+              await instance.callStatic['toHexString(uint256)'](
+                ethers.BigNumber.from(inputValues[i]),
+              ),
+            ).to.equal(outputValues[i]);
+          }
+        });
       });
 
       describe('(uint256,uint256)', function () {
-        it('todo');
+        it('Fails if length input is 0 and value is nonzero', async () => {
+          await expect(
+            instance.callStatic['toHexString(uint256,uint256)'](
+              ethers.BigNumber.from('100'),
+              ethers.BigNumber.from('0'),
+            ),
+          ).to.be.revertedWith('Strings: hex length insufficient');
+        });
+
+        it('Should return correct hexString representation for matching value and length pairs', async () => {
+          const inputValues = [
+            '1000',
+            '1',
+            '12345',
+            '85746201361230',
+            '999983',
+          ];
+          const inputLengths = ['2', '1', '2', '6', '3'];
+          const outputValues = [
+            '0x03e8',
+            '0x01',
+            '0x3039',
+            '0x4dfc57df7b4e',
+            '0x0f422f',
+          ];
+
+          for (let i = 0; i < inputValues.length; i++) {
+            expect(
+              await instance.callStatic['toHexString(uint256,uint256)'](
+                ethers.BigNumber.from(inputValues[i]),
+                ethers.BigNumber.from(inputLengths[i]),
+              ),
+            ).to.equal(outputValues[i]);
+          }
+        });
       });
     });
   });
