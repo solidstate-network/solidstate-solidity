89fa83c9bf7b717c413887259bce4ebde5e8ad94
Nick Barry <itsnickbarry@protonmail.ch>
2021-12-19 16:18:28 -0600

    update hardhat-abi-exporter configuration, remove obsolete compile task override

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 
 import { task } from 'hardhat/config';
-import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 import 'hardhat-abi-exporter';
@@ -12,24 +11,6 @@ import 'solidity-coverage';
 import Dotenv from 'dotenv';
 
 Dotenv.config();
-
-task(TASK_COMPILE, async function (args, hre, runSuper) {
-  // preserve @solidstate/abi package data when ABI export directory is cleared
-
-  const data = ['package.json', 'README.md'].map(function (name) {
-    const filePath = path.resolve(hre.config.abiExporter.path, name);
-    return {
-      filePath,
-      contents: fs.readFileSync(filePath, 'utf8'),
-    };
-  });
-
-  await runSuper();
-
-  data.forEach(function ({ filePath, contents }) {
-    fs.writeFileSync(filePath, contents);
-  });
-});
 
 export default {
   solidity: {
@@ -53,6 +34,7 @@ export default {
   },
 
   abiExporter: {
+    runOnCompile: true,
     clear: true,
     flat: true,
     except: ['.*Mock$'],
