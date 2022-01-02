452bc317c4c933bfbc59bdee85276a17b7949276
Nick Barry <itsnickbarry@protonmail.ch>
2022-01-02 01:04:51 -0600

    optimize MerkleProof with unchecked for loop

--- contracts/cryptography/MerkleProof.sol
+++ contracts/cryptography/MerkleProof.sol
@@ -19,22 +19,24 @@ library MerkleProof {
         bytes32 root,
         bytes32 leaf
     ) internal pure returns (bool) {
-        bytes32 computedHash = leaf;
+        unchecked {
+            bytes32 computedHash = leaf;
 
-        for (uint256 i = 0; i < proof.length; i++) {
-            bytes32 proofElement = proof[i];
+            for (uint256 i = 0; i < proof.length; i++) {
+                bytes32 proofElement = proof[i];
 
-            if (computedHash <= proofElement) {
-                computedHash = keccak256(
-                    abi.encodePacked(computedHash, proofElement)
-                );
-            } else {
-                computedHash = keccak256(
-                    abi.encodePacked(proofElement, computedHash)
-                );
+                if (computedHash <= proofElement) {
+                    computedHash = keccak256(
+                        abi.encodePacked(computedHash, proofElement)
+                    );
+                } else {
+                    computedHash = keccak256(
+                        abi.encodePacked(proofElement, computedHash)
+                    );
+                }
             }
-        }
 
-        return computedHash == root;
+            return computedHash == root;
+        }
     }
 }
