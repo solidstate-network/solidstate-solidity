80c8d6c4f8bf78f627ac78e4cb730154f197bf9c
Nick Barry <itsnickbarry@protonmail.ch>
2022-01-27 20:04:30 -0600

    add UpgradeableProxyOwnable#setImplementation

--- contracts/proxy/upgradeable/UpgradeableProxyOwnable.sol
+++ contracts/proxy/upgradeable/UpgradeableProxyOwnable.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.8.0;
 import { UpgradeableProxy } from './UpgradeableProxy.sol';
 import { OwnableInternal } from '../../access/OwnableInternal.sol';
 
-abstract contract UpgradeableProxyOwnable is
-    UpgradeableProxy,
-    OwnableInternal
-{}
+abstract contract UpgradeableProxyOwnable is UpgradeableProxy, OwnableInternal {
+    function setImplementation(address implementation) external onlyOwner {
+        _setImplementation(implementation);
+    }
+}
