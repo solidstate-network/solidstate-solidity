81af965593f529cff86b8a84b6f32abcab0dfcd4
Nick Barry <itsnickbarry@protonmail.ch>
2022-01-27 22:19:46 -0600

    add NatSpec comments to UpgradeableProxyOwnable

--- contracts/proxy/upgradeable/UpgradeableProxyOwnable.sol
+++ contracts/proxy/upgradeable/UpgradeableProxyOwnable.sol
@@ -5,7 +5,14 @@ pragma solidity ^0.8.0;
 import { UpgradeableProxy } from './UpgradeableProxy.sol';
 import { OwnableInternal } from '../../access/OwnableInternal.sol';
 
+/**
+ * @title Proxy with upgradeable implementation controlled by ERC171 owner
+ */
 abstract contract UpgradeableProxyOwnable is UpgradeableProxy, OwnableInternal {
+    /**
+     * @notice set logic implementation address
+     * @param implementation implementation address
+     */
     function setImplementation(address implementation) external onlyOwner {
         _setImplementation(implementation);
     }
