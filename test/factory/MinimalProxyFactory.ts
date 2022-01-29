2b4ece868067b5b2a6ed117475ad3807ccad64ff
NouDaimon <peric.iason@gmail.com>
2022-01-29 12:38:23 +0200

    overload separation | MinimalProxyFactory

--- test/factory/MinimalProxyFactory.ts
+++ test/factory/MinimalProxyFactory.ts
@@ -17,71 +17,66 @@ describe('MinimalProxyFactory', function () {
   describeBehaviorOfMinimalProxyFactory({ deploy: async () => instance });
 
   describe('__internal', function () {
-    describe('#_deployMinimalProxy', function () {
-      describe('(address)', function () {
-        it('deploys minimal proxy and returns deployment address', async function () {
-          const target = instance.address;
+    describe('#_deployMinimalProxy(address)', function () {
+      it('deploys minimal proxy and returns deployment address', async function () {
+        const target = instance.address;
 
-          const address = await instance.callStatic[
-            '__deployMinimalProxy(address)'
-          ](target);
-          expect(address).to.be.properAddress;
+        const address = await instance.callStatic[
+          '__deployMinimalProxy(address)'
+        ](target);
+        expect(address).to.be.properAddress;
 
-          await instance['__deployMinimalProxy(address)'](target);
+        await instance['__deployMinimalProxy(address)'](target);
 
-          expect(await ethers.provider.getCode(address)).to.equal(
-            '0x' +
-              [
-                '363d3d373d3d3d363d73',
-                target.replace('0x', '').toLowerCase(),
-                '5af43d82803e903d91602b57fd5bf3',
-              ].join(''),
-          );
-        });
-
-        describe('reverts if', function () {
-          it('contract creation fails');
-        });
+        expect(await ethers.provider.getCode(address)).to.equal(
+          '0x' +
+            [
+              '363d3d373d3d3d363d73',
+              target.replace('0x', '').toLowerCase(),
+              '5af43d82803e903d91602b57fd5bf3',
+            ].join(''),
+        );
       });
 
-      describe('(address,bytes32)', function () {
-        it('deploys minimal proxy and returns deployment address', async function () {
+      describe('reverts if', function () {
+        it('contract creation fails');
+      });
+    });
+
+    describe('#_deployMinimalProxy(address,bytes32)', function () {
+      it('deploys minimal proxy and returns deployment address', async function () {
+        const target = instance.address;
+        const salt = ethers.utils.randomBytes(32);
+
+        const address = await instance.callStatic[
+          '__deployMinimalProxy(address,bytes32)'
+        ](target, salt);
+        expect(address).to.be.properAddress;
+
+        await instance['__deployMinimalProxy(address,bytes32)'](target, salt);
+
+        expect(await ethers.provider.getCode(address)).to.equal(
+          '0x' +
+            [
+              '363d3d373d3d3d363d73',
+              target.replace('0x', '').toLowerCase(),
+              '5af43d82803e903d91602b57fd5bf3',
+            ].join(''),
+        );
+      });
+
+      describe('reverts if', function () {
+        it('contract creation fails');
+
+        it('salt has already been used', async function () {
           const target = instance.address;
           const salt = ethers.utils.randomBytes(32);
 
-          const address = await instance.callStatic[
-            '__deployMinimalProxy(address,bytes32)'
-          ](target, salt);
-          expect(address).to.be.properAddress;
-
           await instance['__deployMinimalProxy(address,bytes32)'](target, salt);
 
-          expect(await ethers.provider.getCode(address)).to.equal(
-            '0x' +
-              [
-                '363d3d373d3d3d363d73',
-                target.replace('0x', '').toLowerCase(),
-                '5af43d82803e903d91602b57fd5bf3',
-              ].join(''),
-          );
-        });
-
-        describe('reverts if', function () {
-          it('contract creation fails');
-
-          it('salt has already been used', async function () {
-            const target = instance.address;
-            const salt = ethers.utils.randomBytes(32);
-
-            await instance['__deployMinimalProxy(address,bytes32)'](
-              target,
-              salt,
-            );
-
-            await expect(
-              instance['__deployMinimalProxy(address,bytes32)'](target, salt),
-            ).to.be.revertedWith('Factory: failed deployment');
-          });
+          await expect(
+            instance['__deployMinimalProxy(address,bytes32)'](target, salt),
+          ).to.be.revertedWith('Factory: failed deployment');
         });
       });
     });
