f7d784e27639fc6e04859a75e0b089ec350ad53b
NouDaimon <peric.iason@gmail.com>
2022-01-30 12:38:48 +0200

    added signatures to access spec | all

--- spec/access/SafeOwnable.behavior.ts
+++ spec/access/SafeOwnable.behavior.ts
@@ -55,7 +55,7 @@ export function describeBehaviorOfSafeOwnable(
       });
     });
 
-    describe('#transferOwnership', function () {
+    describe('#transferOwnership(address)', function () {
       it('does not set new owner', async function () {
         await instance
           .connect(owner)
@@ -74,7 +74,7 @@ export function describeBehaviorOfSafeOwnable(
       });
     });
 
-    describe('#acceptOwnership', function () {
+    describe('#acceptOwnership()', function () {
       it('sets new owner', async function () {
         await instance
           .connect(owner)
