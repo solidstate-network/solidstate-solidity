c1a4f20dd5f23e8223a00b033353ff143cb4a802
Nick Barry <itsnickbarry@protonmail.ch>
2022-03-20 17:47:45 -0600

    add IERC4626Internal interface, update ABIs

--- contracts/token/ERC4626/IERC4626.sol
+++ contracts/token/ERC4626/IERC4626.sol
@@ -3,26 +3,13 @@
 pragma solidity ^0.8.0;
 
 import { IERC20 } from '../ERC20/IERC20.sol';
+import { IERC4626Internal } from './IERC4626Internal.sol';
 
 /**
  * @title ERC4626 interface
  * @dev see https://github.com/ethereum/EIPs/issues/4626
  */
-interface IERC4626 is IERC20 {
-    event Deposit(
-        address indexed caller,
-        address indexed owner,
-        uint256 assets,
-        uint256 shares
-    );
-
-    event Withdraw(
-        address indexed caller,
-        address indexed owner,
-        uint256 assets,
-        uint256 shares
-    );
-
+interface IERC4626 is IERC4626Internal, IERC20 {
     /**
      * @notice get the address of the base token used for vault accountin purposes
      * @return base token address
---
+++ contracts/token/ERC4626/IERC4626Internal.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title Partial ERC4626 interface needed by internal functions
+ */
+interface IERC4626Internal {
+    event Deposit(
+        address indexed caller,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+
+    event Withdraw(
+        address indexed caller,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+}
