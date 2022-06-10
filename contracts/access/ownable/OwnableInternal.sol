54d77f28006f36bc5a7785b45ac486f710940d24
Nick Barry <itsnickbarry@protonmail.ch>
2022-06-10 16:14:02 +0300

    add transitive owner concept to OwnableInternal

--- contracts/access/ownable/OwnableInternal.sol
+++ contracts/access/ownable/OwnableInternal.sol
@@ -2,10 +2,13 @@
 
 pragma solidity ^0.8.0;
 
+import { AddressUtils } from '../../utils/AddressUtils.sol';
+import { IERC173 } from '../IERC173.sol';
 import { IOwnableInternal } from './IOwnableInternal.sol';
 import { OwnableStorage } from './OwnableStorage.sol';
 
 abstract contract OwnableInternal is IOwnableInternal {
+    using AddressUtils for address;
     using OwnableStorage for OwnableStorage.Layout;
 
     modifier onlyOwner() {
@@ -16,8 +19,30 @@ abstract contract OwnableInternal is IOwnableInternal {
         _;
     }
 
+    modifier onlyTransitiveOwner() {
+        require(
+            msg.sender == _transitiveOwner(),
+            'Ownable: sender must be transitive owner'
+        );
+        _;
+    }
+
     function _owner() internal view virtual returns (address) {
         return OwnableStorage.layout().owner;
+    }
+
+    function _transitiveOwner() internal view virtual returns (address) {
+        address owner = _owner();
+
+        while (owner.isContract()) {
+            try IERC173(owner).owner() returns (address transitiveOwner) {
+                owner = transitiveOwner;
+            } catch {
+                // do nothing
+            }
+        }
+
+        return owner;
     }
 
     function _transferOwnership(address account) internal virtual {
