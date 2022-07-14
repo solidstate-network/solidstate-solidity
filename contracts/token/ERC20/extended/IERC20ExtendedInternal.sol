1a46a0ed34ca8efdfa110ed72f10a7b460bbf0a2
Nick Barry <itsnickbarry@protonmail.ch>
2022-07-15 02:06:31 +0300

    add internal ERC20Extended contract and interface

---
+++ contracts/token/ERC20/extended/IERC20ExtendedInternal.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+import { IERC20Internal } from '../IERC20Internal.sol';
+
+/**
+ * @title ERC20 extended internal interface
+ */
+interface IERC20ExtendedInternal is IERC20Internal {
+
+}
