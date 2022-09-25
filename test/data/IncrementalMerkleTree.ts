141f53b3e81740ce742a43b671deeef1c4ee9697
Nick Barry <itsnickbarry@protonmail.ch>
2022-09-25 17:44:18 -0400

    test root function

--- test/data/IncrementalMerkleTree.ts
+++ test/data/IncrementalMerkleTree.ts
@@ -41,88 +41,23 @@ describe('IncrementalMerkleTree', function () {
     });
   });
 
-  it('todo', async () => {
-    const letters = ['a', 'b', 'c', 'd', 'e'];
+  describe('#root', () => {
+    it('returns Merkle root', async () => {
+      const values = [];
 
-    const data = {};
+      for (let i = 0; i < 10; i++) {
+        values.push(String(Math.random()));
+      }
 
-    for (const d of letters) {
-      data[d] = ethers.utils.solidityKeccak256(
-        ['bytes'],
-        [ethers.utils.toUtf8Bytes(d)],
-      );
-    }
+      for (let i = 0; i < values.length; i++) {
+        await instance.push(ethers.utils.toUtf8Bytes(values[i]));
 
-    const hash_ab = ethers.utils.solidityKeccak256(
-      ['bytes32', 'bytes32'],
-      [data.a, data.b],
-    );
+        const tree = new MerkleTree(values.slice(0, i + 1), keccak256, {
+          hashLeaves: true,
+        });
 
-    const hash_cd = ethers.utils.solidityKeccak256(
-      ['bytes32', 'bytes32'],
-      [data.c, data.d],
-    );
-
-    const hash_ab_c = ethers.utils.solidityKeccak256(
-      ['bytes32', 'bytes32'],
-      [hash_ab, data.c],
-    );
-
-    const hash_ab_cd = ethers.utils.solidityKeccak256(
-      ['bytes32', 'bytes32'],
-      [hash_ab, hash_cd],
-    );
-
-    const hash_abcd_e = ethers.utils.solidityKeccak256(
-      ['bytes32', 'bytes32'],
-      [hash_ab_cd, data.e],
-    );
-
-    await instance.push(ethers.utils.toUtf8Bytes('a'));
-    expect(await instance.callStatic.root()).to.equal(data.a);
-    expect(await instance.callStatic.root()).to.equal(
-      new MerkleTree(['a'], keccak256, {
-        hashLeaves: true,
-      }).getHexRoot(),
-    );
-
-    await instance.push(ethers.utils.toUtf8Bytes('b'));
-    expect(await instance.callStatic.root()).to.equal(hash_ab);
-    expect(await instance.callStatic.root()).to.equal(
-      new MerkleTree(['a', 'b'], keccak256, {
-        hashLeaves: true,
-      }).getHexRoot(),
-    );
-
-    await instance.push(ethers.utils.toUtf8Bytes('c'));
-    expect(await instance.callStatic.root()).to.equal(hash_ab_c);
-    expect(await instance.callStatic.root()).to.equal(
-      new MerkleTree(['a', 'b', 'c'], keccak256, {
-        hashLeaves: true,
-      }).getHexRoot(),
-    );
-
-    await instance.push(ethers.utils.toUtf8Bytes('d'));
-    expect(await instance.callStatic.root()).to.equal(hash_ab_cd);
-    expect(await instance.callStatic.root()).to.equal(
-      new MerkleTree(['a', 'b', 'c', 'd'], keccak256, {
-        hashLeaves: true,
-      }).getHexRoot(),
-    );
-
-    await instance.push(ethers.utils.toUtf8Bytes('e'));
-    expect(await instance.callStatic.root()).to.equal(hash_abcd_e);
-    expect(await instance.callStatic.root()).to.equal(
-      new MerkleTree(['a', 'b', 'c', 'd', 'e'], keccak256, {
-        hashLeaves: true,
-      }).getHexRoot(),
-    );
-
-    await instance.set(1, ethers.utils.toUtf8Bytes('e'));
-    expect(await instance.callStatic.root()).to.equal(
-      new MerkleTree(['a', 'e', 'c', 'd', 'e'], keccak256, {
-        hashLeaves: true,
-      }).getHexRoot(),
-    );
+        expect(await instance.callStatic.root()).to.equal(tree.getHexRoot());
+      }
+    });
   });
 });
