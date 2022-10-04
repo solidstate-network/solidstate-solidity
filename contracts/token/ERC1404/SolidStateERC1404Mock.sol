41e9ba70421230782f532347025cda338b496844
Nick Barry <itsnickbarry@protonmail.ch>
2022-10-04 19:01:02 -0400

    fix ERC1404 visibility layers and move custom errors to internal interface

--- contracts/token/ERC1404/SolidStateERC1404Mock.sol
+++ contracts/token/ERC1404/SolidStateERC1404Mock.sol
@@ -3,13 +3,10 @@
 pragma solidity ^0.8.8;
 
 import { SolidStateERC1404 } from './SolidStateERC1404.sol';
-import { ERC1404BaseStorage } from './base/ERC1404BaseStorage.sol';
 
 contract SolidStateERC1404Mock is SolidStateERC1404 {
-    using ERC1404BaseStorage for ERC1404BaseStorage.Layout;
-
     constructor(uint8[] memory errorCodes, string[] memory errorMessages) {
-        ERC1404BaseStorage.layout().setRestrictions(errorCodes, errorMessages);
+        _setRestrictions(errorCodes, errorMessages);
     }
 
     function _detectTransferRestriction(
--- contracts/token/ERC1404/base/ERC1404BaseStorage.sol
+++ contracts/token/ERC1404/base/ERC1404BaseStorage.sol
@@ -3,8 +3,6 @@
 pragma solidity ^0.8.8;
 
 library ERC1404BaseStorage {
-    error ERC1404BaseStorage__ArrayLengthMismatch();
-
     struct Layout {
         mapping(uint8 => string) restrictions;
     }
@@ -17,30 +15,5 @@ library ERC1404BaseStorage {
         assembly {
             l.slot := slot
         }
-    }
-
-    function setRestrictions(
-        Layout storage l,
-        uint8[] memory restrictionCodes,
-        string[] memory restrictionMessages
-    ) internal {
-        if (restrictionCodes.length != restrictionMessages.length)
-            revert ERC1404BaseStorage__ArrayLengthMismatch();
-
-        mapping(uint8 => string) storage restrictions = l.restrictions;
-
-        unchecked {
-            for (uint256 i; i < restrictionCodes.length; i++) {
-                restrictions[restrictionCodes[i]] = restrictionMessages[i];
-            }
-        }
-    }
-
-    function getRestrictionMessage(Layout storage l, uint8 restrictionCode)
-        internal
-        view
-        returns (string memory)
-    {
-        return l.restrictions[restrictionCode];
     }
 }
