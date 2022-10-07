c5657dc86a9e7ccaa6d28c351925614a8c75b28d
0xCourtney <courtney@scottworks.io>
2022-10-07 16:19:02 -0400

    adds address mock and tests

--- test/data/EnumerableSet.ts
+++ test/data/EnumerableSet.ts
@@ -1,12 +1,15 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { bnToBytes32 } from '@solidstate/library';
+import { bnToBytes32, bnToAddress } from '@solidstate/library';
 import {
   EnumerableSetBytes32Mock,
   EnumerableSetBytes32Mock__factory,
+  EnumerableSetAddressMock,
+  EnumerableSetAddressMock__factory,
   EnumerableSetUintMock,
   EnumerableSetUintMock__factory,
 } from '@solidstate/typechain-types';
 import { expect } from 'chai';
+import { BigNumber } from 'ethers';
 import { ethers } from 'hardhat';
 
 describe('EnumerableSet', async () => {
@@ -198,6 +201,194 @@ describe('EnumerableSet', async () => {
     });
   });
 
+  describe('AddressSet', async () => {
+    let instance: EnumerableSetAddressMock;
+    let deployer: SignerWithAddress;
+
+    beforeEach(async () => {
+      [deployer] = await ethers.getSigners();
+      instance = await new EnumerableSetAddressMock__factory(deployer).deploy();
+    });
+
+    describe('__internal', () => {
+      const zeroAddress = bnToAddress(BigNumber.from(0));
+      const oneAddress = bnToAddress(BigNumber.from(1));
+      const twoAddress = bnToAddress(BigNumber.from(2));
+
+      describe('#at(uint256)', () => {
+        it('returns the value corresponding to index provided', async () => {
+          await instance['add(address)'](zeroAddress);
+          await instance['add(address)'](twoAddress);
+          await instance['add(address)'](oneAddress);
+
+          expect(await instance['at(uint256)'](0)).to.equal(zeroAddress);
+          expect(await instance['at(uint256)'](1)).to.equal(twoAddress);
+          expect(await instance['at(uint256)'](2)).to.equal(oneAddress);
+        });
+
+        describe('reverts if', function () {
+          it('index out of bounds', async () => {
+            await expect(
+              instance['at(uint256)'](0),
+            ).to.be.revertedWithCustomError(
+              instance,
+              'EnumerableSet__IndexOutOfBounds',
+            );
+          });
+        });
+      });
+
+      describe('#contains(address)', () => {
+        it('returns true if the value has been added', async () => {
+          await instance['add(address)'](zeroAddress);
+          await instance['add(address)'](twoAddress);
+          await instance['add(address)'](oneAddress);
+
+          expect(await instance['contains(address)'](zeroAddress)).to.be.true;
+          expect(await instance['contains(address)'](oneAddress)).to.be.true;
+          expect(await instance['contains(address)'](twoAddress)).to.be.true;
+        });
+
+        it('returns false if the value has not been added', async () => {
+          expect(await instance['contains(address)'](zeroAddress)).to.be.false;
+        });
+      });
+
+      describe('#indexOf(address)', () => {
+        it('returns index of the value', async () => {
+          await instance['add(address)'](zeroAddress);
+          await instance['add(address)'](twoAddress);
+          await instance['add(address)'](oneAddress);
+
+          expect(await instance['indexOf(address)'](zeroAddress)).to.equal(0);
+          expect(await instance['indexOf(address)'](oneAddress)).to.equal(2);
+          expect(await instance['indexOf(address)'](twoAddress)).to.equal(1);
+        });
+
+        it('returns max address if value does not exist', async () => {
+          expect(await instance['indexOf(address)'](zeroAddress)).to.equal(
+            ethers.constants.MaxUint256.toString(),
+          );
+        });
+      });
+
+      describe('#length()', () => {
+        it('returns length of enumerable set', async () => {
+          expect(await instance['length()']()).to.equal(0);
+
+          await instance['add(address)'](zeroAddress);
+          expect(await instance['length()']()).to.equal(1);
+
+          await instance['add(address)'](oneAddress);
+          expect(await instance['length()']()).to.equal(2);
+
+          await instance['add(address)'](twoAddress);
+          expect(await instance['length()']()).to.equal(3);
+
+          await instance['remove(address)'](twoAddress);
+          expect(await instance['length()']()).to.equal(2);
+
+          await instance['remove(address)'](oneAddress);
+          expect(await instance['length()']()).to.equal(1);
+
+          await instance['remove(address)'](zeroAddress);
+          expect(await instance['length()']()).to.equal(0);
+        });
+      });
+
+      describe('#add(address)', () => {
+        it('adds value to set', async () => {
+          await instance['add(address)'](zeroAddress);
+          await instance['add(address)'](oneAddress);
+          await instance['add(address)'](twoAddress);
+
+          expect(await instance['toArray()']()).to.deep.equal([
+            zeroAddress,
+            oneAddress,
+            twoAddress,
+          ]);
+        });
+
+        it('returns true if value is added', async () => {
+          expect(await instance.callStatic['add(address)'](zeroAddress)).to.be
+            .true;
+        });
+
+        it('returns false if value has already been added', async () => {
+          await instance['add(address)'](zeroAddress);
+          expect(await instance.callStatic['add(address)'](zeroAddress)).to.be
+            .false;
+        });
+      });
+
+      describe('#remove(address)', () => {
+        it('removes value from set', async () => {
+          await instance['add(address)'](zeroAddress);
+          await instance['add(address)'](oneAddress);
+          await instance['add(address)'](twoAddress);
+
+          await instance['remove(address)'](zeroAddress);
+          expect(await instance['toArray()']()).to.deep.equal([
+            twoAddress,
+            oneAddress,
+          ]);
+        });
+
+        it('returns true if value is removed', async () => {
+          await instance['add(address)'](zeroAddress);
+          await instance['add(address)'](oneAddress);
+          await instance['add(address)'](twoAddress);
+
+          expect(await instance.callStatic['remove(address)'](zeroAddress)).to
+            .be.true;
+        });
+
+        it('returns false if value is not removed', async () => {
+          expect(await instance.callStatic['remove(address)'](zeroAddress)).to
+            .be.false;
+        });
+
+        it('removes value from index mapping and array', async () => {
+          await instance['add(address)'](zeroAddress);
+          await instance['add(address)'](oneAddress);
+          await instance['add(address)'](twoAddress);
+
+          await instance['remove(address)'](twoAddress);
+          expect(await instance['length()']()).to.be.equal(2);
+          expect(await instance['indexOf(address)'](twoAddress)).to.be.equal(
+            ethers.constants.MaxUint256,
+          );
+
+          await instance['remove(address)'](oneAddress);
+          expect(await instance['length()']()).to.be.equal(1);
+          expect(await instance['indexOf(address)'](oneAddress)).to.be.equal(
+            ethers.constants.MaxUint256,
+          );
+
+          await instance['remove(address)'](zeroAddress);
+          expect(await instance['length()']()).to.be.equal(0);
+          expect(await instance['indexOf(address)'](zeroAddress)).to.be.equal(
+            ethers.constants.MaxUint256,
+          );
+        });
+      });
+
+      describe('#toArray()', () => {
+        it('returns the set as an array', async () => {
+          await instance['add(address)'](zeroAddress);
+          await instance['add(address)'](twoAddress);
+          await instance['add(address)'](oneAddress);
+
+          expect(await instance['toArray()']()).to.deep.equal([
+            zeroAddress,
+            twoAddress,
+            oneAddress,
+          ]);
+        });
+      });
+    });
+  });
+
   describe('UintSet', async () => {
     let instance: EnumerableSetUintMock;
     let deployer: SignerWithAddress;
@@ -293,7 +484,7 @@ describe('EnumerableSet', async () => {
         });
       });
 
-      describe('#add(bytes32)', () => {
+      describe('#add(uint256)', () => {
         it('adds value to set', async () => {
           await instance['add(uint256)'](zero);
           await instance['add(uint256)'](one);
@@ -312,7 +503,7 @@ describe('EnumerableSet', async () => {
         });
       });
 
-      describe('#remove(bytes32)', () => {
+      describe('#remove(uint256)', () => {
         it('removes value from set', async () => {
           await instance['add(uint256)'](zero);
           await instance['add(uint256)'](one);
@@ -357,6 +548,16 @@ describe('EnumerableSet', async () => {
           expect(await instance['indexOf(uint256)'](zero)).to.be.equal(
             ethers.constants.MaxUint256,
           );
+        });
+      });
+
+      describe('#toArray()', () => {
+        it('returns the set as an array', async () => {
+          await instance['add(uint256)'](zero);
+          await instance['add(uint256)'](two);
+          await instance['add(uint256)'](one);
+
+          expect(await instance['toArray()']()).to.deep.equal([zero, two, one]);
         });
       });
     });
