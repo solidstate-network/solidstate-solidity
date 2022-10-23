01e18aef16923aa04fdc07182d4732f600ee1ce7
NouDaimon <peric.iason@gmail.com>
2022-10-23 12:24:26 +0300

    EnumerableMapUintToAddress mock contract

---
+++ contracts/data/EnumerableMapUintToAddressMock.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+import { EnumerableMap } from './EnumerableMap.sol';
+
+contract EnumerableMapUintToAddressMock {
+    using EnumerableMap for EnumerableMap.UintToAddressMap;
+
+    EnumerableMap.UintToAddressMap internal map;
+
+    function at(uint256 index) external view returns (uint256, address) {
+        return map.at(index);
+    }
+
+    function contains(uint256 key) external view returns (bool) {
+        return map.contains(key);
+    }
+
+    function length() external view returns (uint256) {
+        return map.length();
+    }
+
+    function get(uint256 key) external view returns (address) {
+        return map.get(key);
+    }
+
+    function set(uint256 key, address value) external returns (bool) {
+        return map.set(key, value);
+    }
+
+    function remove(uint256 key) external returns (bool) {
+        return map.remove(key);
+    }
+}
