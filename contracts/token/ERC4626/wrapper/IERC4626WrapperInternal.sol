d5af9df57577ebfd81ddb4425bd8be0885607ff1
Nick Barry <itsnickbarry@protonmail.ch>
2022-11-26 13:39:51 -0600

    add draft ERC4626Wrapper contracts

---
+++ contracts/token/ERC4626/wrapper/IERC4626WrapperInternal.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+import { IERC4626BaseInternal } from '../base/IERC4626BaseInternal.sol';
+
+/**
+ * @title ERC4626 wrapper interface
+ */
+interface IERC4626WrapperInternal is IERC4626BaseInternal {
+
+}
