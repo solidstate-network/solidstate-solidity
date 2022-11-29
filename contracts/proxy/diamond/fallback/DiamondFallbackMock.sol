5fe880d2f4ce349ecf9193a03098bea1cb9f89d0
Nick Barry <itsnickbarry@protonmail.ch>
2022-11-29 14:18:53 -0600

    setup DiamondFallback tests

--- contracts/proxy/diamond/fallback/DiamondFallbackMock.sol
+++ contracts/proxy/diamond/fallback/DiamondFallbackMock.sol
@@ -2,11 +2,15 @@
 
 pragma solidity ^0.8.8;
 
+import { OwnableStorage } from '../../../access/ownable/OwnableStorage.sol';
 import { DiamondWritableInternal } from '../writable/DiamondWritableInternal.sol';
 import { DiamondFallback } from './DiamondFallback.sol';
 
 contract DiamondFallbackMock is DiamondFallback, DiamondWritableInternal {
+    using OwnableStorage for OwnableStorage.Layout;
+
     constructor(FacetCut[] memory cuts) {
+        OwnableStorage.layout().setOwner(msg.sender);
         _diamondCut(cuts, address(0), '');
     }
 }
---
+++ spec/proxy/diamond/fallback/index.ts
@@ -0,0 +1,1 @@
+export * from './DiamondFallback.behavior';
