c8195bc74f3ac65fd90b65a3982066fe4b9cffc8
0xCourtney <23440493+0xCourtney@users.noreply.github.com>
2022-12-20 11:46:26 -0500

    moves diamond utils to diamond subdirectory

--- lib/diamond/utils.ts
+++ lib/diamond/utils.ts
@@ -1,3 +1,4 @@
+import { FacetFilter, selectorIsFiltered, validateFilters } from './filters';
 import { AddressZero } from '@ethersproject/constants';
 import { Contract } from '@ethersproject/contracts';
 import {
@@ -53,62 +54,6 @@ export function selectorExistsInFacets(
     if (facet.selectors.includes(selector)) return true;
   }
   return false;
-}
-
-interface FacetFilter {
-  contract: string;
-  selectors: string[];
-}
-
-// returns true if the selector is found in the only or exclude filters
-function selectorIsFiltered(
-  only: FacetFilter[],
-  exclude: FacetFilter[],
-  contract: string,
-  selector: string,
-): boolean {
-  if (only.length > 0) {
-    // include selectors found in only, exclude all others
-    return includes(only, contract, selector);
-  }
-
-  if (exclude.length > 0) {
-    // exclude selectors found in exclude, include all others
-    return !includes(exclude, contract, selector);
-  }
-
-  // if neither only or exclude are used, then include all selectors
-  return true;
-}
-
-// returns true if the selector is found in the filters
-function includes(
-  filters: FacetFilter[],
-  contract: string,
-  selector: string,
-): boolean {
-  for (const filter of filters) {
-    if (filter.contract === contract || AddressZero === contract) {
-      return filter.selectors.includes(selector);
-    }
-  }
-
-  return false;
-}
-
-// validates that only and exclude filters do not contain the same contract
-function validateFilters(only: FacetFilter[], exclude: FacetFilter[]) {
-  if (only.length > 0 && exclude.length > 0) {
-    for (const onlyFilter of only) {
-      for (const excludeFilter of exclude) {
-        if (onlyFilter.contract === excludeFilter.contract) {
-          throw new Error(
-            'only and exclude filters cannot contain the same contract',
-          );
-        }
-      }
-    }
-  }
 }
 
 // preview FacetCut which adds unregistered selectors
