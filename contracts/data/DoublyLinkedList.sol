8caafb40a5979f915d240ac3305c9a0b42456f4d
Nick Barry <itsnickbarry@protonmail.ch>
2022-12-23 21:26:04 +0200

    remove queue references

--- contracts/data/DoublyLinkedList.sol
+++ contracts/data/DoublyLinkedList.sol
@@ -26,353 +26,353 @@ library DoublyLinkedList {
     error DoublyLinkedList__InvalidValue();
 
     function contains(
-        Bytes32DoublyLinkedList storage queue,
+        Bytes32DoublyLinkedList storage self,
         bytes32 value
     ) internal view returns (bool) {
-        return _contains(queue._inner, value);
+        return _contains(self._inner, value);
     }
 
     function contains(
-        AddressDoublyLinkedList storage queue,
+        AddressDoublyLinkedList storage self,
         address value
     ) internal view returns (bool) {
-        return _contains(queue._inner, bytes32(uint256(uint160(value))));
+        return _contains(self._inner, bytes32(uint256(uint160(value))));
     }
 
     function contains(
-        UintDoublyLinkedList storage queue,
+        UintDoublyLinkedList storage self,
         uint256 value
     ) internal view returns (bool) {
-        return _contains(queue._inner, bytes32(value));
+        return _contains(self._inner, bytes32(value));
     }
 
     function prev(
-        Bytes32DoublyLinkedList storage queue,
+        Bytes32DoublyLinkedList storage self,
         bytes32 value
     ) internal view returns (bytes32) {
-        return _prev(queue._inner, value);
+        return _prev(self._inner, value);
     }
 
     function prev(
-        AddressDoublyLinkedList storage queue,
+        AddressDoublyLinkedList storage self,
         address value
     ) internal view returns (address) {
         return
             address(
                 uint160(
                     uint256(
-                        _prev(queue._inner, bytes32(uint256(uint160(value))))
+                        _prev(self._inner, bytes32(uint256(uint160(value))))
                     )
                 )
             );
     }
 
     function prev(
-        UintDoublyLinkedList storage queue,
+        UintDoublyLinkedList storage self,
         uint256 value
     ) internal view returns (uint256) {
-        return uint256(_prev(queue._inner, bytes32(value)));
+        return uint256(_prev(self._inner, bytes32(value)));
     }
 
     function next(
-        Bytes32DoublyLinkedList storage queue,
+        Bytes32DoublyLinkedList storage self,
         bytes32 value
     ) internal view returns (bytes32) {
-        return _next(queue._inner, value);
+        return _next(self._inner, value);
     }
 
     function next(
-        AddressDoublyLinkedList storage queue,
+        AddressDoublyLinkedList storage self,
         address value
     ) internal view returns (address) {
         return
             address(
                 uint160(
                     uint256(
-                        _next(queue._inner, bytes32(uint256(uint160(value))))
+                        _next(self._inner, bytes32(uint256(uint160(value))))
                     )
                 )
             );
     }
 
     function next(
-        UintDoublyLinkedList storage queue,
+        UintDoublyLinkedList storage self,
         uint256 value
     ) internal view returns (uint256) {
-        return uint256(_next(queue._inner, bytes32(value)));
+        return uint256(_next(self._inner, bytes32(value)));
     }
 
     function insertBefore(
-        Bytes32DoublyLinkedList storage queue,
+        Bytes32DoublyLinkedList storage self,
         bytes32 nextValue,
         bytes32 newValue
     ) internal returns (bool status) {
-        status = _insertBefore(queue._inner, nextValue, newValue);
+        status = _insertBefore(self._inner, nextValue, newValue);
     }
 
     function insertBefore(
-        AddressDoublyLinkedList storage queue,
+        AddressDoublyLinkedList storage self,
         address nextValue,
         address newValue
     ) internal returns (bool status) {
         status = _insertBefore(
-            queue._inner,
+            self._inner,
             bytes32(uint256(uint160(nextValue))),
             bytes32(uint256(uint160(newValue)))
         );
     }
 
     function insertBefore(
-        UintDoublyLinkedList storage queue,
+        UintDoublyLinkedList storage self,
         uint256 nextValue,
         uint256 newValue
     ) internal returns (bool status) {
         status = _insertBefore(
-            queue._inner,
+            self._inner,
             bytes32(nextValue),
             bytes32(newValue)
         );
     }
 
     function insertAfter(
-        Bytes32DoublyLinkedList storage queue,
+        Bytes32DoublyLinkedList storage self,
         bytes32 prevValue,
         bytes32 newValue
     ) internal returns (bool status) {
-        status = _insertAfter(queue._inner, prevValue, newValue);
+        status = _insertAfter(self._inner, prevValue, newValue);
     }
 
     function insertAfter(
-        AddressDoublyLinkedList storage queue,
+        AddressDoublyLinkedList storage self,
         address prevValue,
         address newValue
     ) internal returns (bool status) {
         status = _insertAfter(
-            queue._inner,
+            self._inner,
             bytes32(uint256(uint160(prevValue))),
             bytes32(uint256(uint160(newValue)))
         );
     }
 
     function insertAfter(
-        UintDoublyLinkedList storage queue,
+        UintDoublyLinkedList storage self,
         uint256 prevValue,
         uint256 newValue
     ) internal returns (bool status) {
         status = _insertAfter(
-            queue._inner,
+            self._inner,
             bytes32(prevValue),
             bytes32(newValue)
         );
     }
 
     function push(
-        Bytes32DoublyLinkedList storage queue,
+        Bytes32DoublyLinkedList storage self,
         bytes32 value
     ) internal returns (bool status) {
-        status = _push(queue._inner, value);
+        status = _push(self._inner, value);
     }
 
     function push(
-        AddressDoublyLinkedList storage queue,
+        AddressDoublyLinkedList storage self,
         address value
     ) internal returns (bool status) {
-        status = _push(queue._inner, bytes32(uint256(uint160(value))));
+        status = _push(self._inner, bytes32(uint256(uint160(value))));
     }
 
     function push(
-        UintDoublyLinkedList storage queue,
+        UintDoublyLinkedList storage self,
         uint256 value
     ) internal returns (bool status) {
-        status = _push(queue._inner, bytes32(value));
+        status = _push(self._inner, bytes32(value));
     }
 
     function pop(
-        Bytes32DoublyLinkedList storage queue
+        Bytes32DoublyLinkedList storage self
     ) internal returns (bytes32 value) {
-        value = _pop(queue._inner);
+        value = _pop(self._inner);
     }
 
     function pop(
-        AddressDoublyLinkedList storage queue
+        AddressDoublyLinkedList storage self
     ) internal returns (address value) {
-        value = address(uint160(uint256(_pop(queue._inner))));
+        value = address(uint160(uint256(_pop(self._inner))));
     }
 
     function pop(
-        UintDoublyLinkedList storage queue
+        UintDoublyLinkedList storage self
     ) internal returns (uint256 value) {
-        value = uint256(_pop(queue._inner));
+        value = uint256(_pop(self._inner));
     }
 
     function shift(
-        Bytes32DoublyLinkedList storage queue
+        Bytes32DoublyLinkedList storage self
     ) internal returns (bytes32 value) {
-        value = _shift(queue._inner);
+        value = _shift(self._inner);
     }
 
     function shift(
-        AddressDoublyLinkedList storage queue
+        AddressDoublyLinkedList storage self
     ) internal returns (address value) {
-        value = address(uint160(uint256(_shift(queue._inner))));
+        value = address(uint160(uint256(_shift(self._inner))));
     }
 
     function shift(
-        UintDoublyLinkedList storage queue
+        UintDoublyLinkedList storage self
     ) internal returns (uint256 value) {
-        value = uint256(_shift(queue._inner));
+        value = uint256(_shift(self._inner));
     }
 
     function unshift(
-        Bytes32DoublyLinkedList storage queue,
+        Bytes32DoublyLinkedList storage self,
         bytes32 value
     ) internal returns (bool status) {
-        status = _unshift(queue._inner, value);
+        status = _unshift(self._inner, value);
     }
 
     function unshift(
-        AddressDoublyLinkedList storage queue,
+        AddressDoublyLinkedList storage self,
         address value
     ) internal returns (bool status) {
-        status = _unshift(queue._inner, bytes32(uint256(uint160(value))));
+        status = _unshift(self._inner, bytes32(uint256(uint160(value))));
     }
 
     function unshift(
-        UintDoublyLinkedList storage queue,
+        UintDoublyLinkedList storage self,
         uint256 value
     ) internal returns (bool status) {
-        status = _unshift(queue._inner, bytes32(value));
+        status = _unshift(self._inner, bytes32(value));
     }
 
     function remove(
-        Bytes32DoublyLinkedList storage queue,
+        Bytes32DoublyLinkedList storage self,
         bytes32 value
     ) internal returns (bool status) {
-        status = _remove(queue._inner, value);
+        status = _remove(self._inner, value);
     }
 
     function remove(
-        AddressDoublyLinkedList storage queue,
+        AddressDoublyLinkedList storage self,
         address value
     ) internal returns (bool status) {
-        status = _remove(queue._inner, bytes32(uint256(uint160(value))));
+        status = _remove(self._inner, bytes32(uint256(uint160(value))));
     }
 
     function remove(
-        UintDoublyLinkedList storage queue,
+        UintDoublyLinkedList storage self,
         uint256 value
     ) internal returns (bool status) {
-        status = _remove(queue._inner, bytes32(value));
+        status = _remove(self._inner, bytes32(value));
     }
 
     function _contains(
-        DoublyLinkedListInternal storage queue,
+        DoublyLinkedListInternal storage self,
         bytes32 value
     ) private view returns (bool) {
-        return queue._asc[value] != 0 || queue._desc[0] == value;
+        return self._asc[value] != 0 || self._desc[0] == value;
     }
 
     function _prev(
-        DoublyLinkedListInternal storage queue,
+        DoublyLinkedListInternal storage self,
         bytes32 value
     ) private view returns (bytes32) {
-        return queue._desc[value];
+        return self._desc[value];
     }
 
     function _next(
-        DoublyLinkedListInternal storage queue,
+        DoublyLinkedListInternal storage self,
         bytes32 value
     ) private view returns (bytes32) {
-        return queue._asc[value];
+        return self._asc[value];
     }
 
     function _insertBefore(
-        DoublyLinkedListInternal storage queue,
+        DoublyLinkedListInternal storage self,
         bytes32 nextValue,
         bytes32 newValue
     ) private returns (bool status) {
         status = _insertBetween(
-            queue,
-            _prev(queue, nextValue),
+            self,
+            _prev(self, nextValue),
             nextValue,
             newValue
         );
     }
 
     function _insertAfter(
-        DoublyLinkedListInternal storage queue,
+        DoublyLinkedListInternal storage self,
         bytes32 prevValue,
         bytes32 newValue
     ) private returns (bool status) {
         status = _insertBetween(
-            queue,
+            self,
             prevValue,
-            _next(queue, prevValue),
+            _next(self, prevValue),
             newValue
         );
     }
 
     function _insertBetween(
-        DoublyLinkedListInternal storage queue,
+        DoublyLinkedListInternal storage self,
         bytes32 prevValue,
         bytes32 nextValue,
         bytes32 newValue
     ) private returns (bool status) {
         if (newValue == bytes32(0)) revert DoublyLinkedList__InvalidValue();
 
-        if (!_contains(queue, newValue)) {
-            _link(queue, prevValue, newValue);
-            _link(queue, newValue, nextValue);
+        if (!_contains(self, newValue)) {
+            _link(self, prevValue, newValue);
+            _link(self, newValue, nextValue);
             status = true;
         }
     }
 
     function _push(
-        DoublyLinkedListInternal storage queue,
+        DoublyLinkedListInternal storage self,
         bytes32 value
     ) private returns (bool status) {
-        status = _insertBetween(queue, _prev(queue, 0), 0, value);
+        status = _insertBetween(self, _prev(self, 0), 0, value);
     }
 
     function _pop(
-        DoublyLinkedListInternal storage queue
+        DoublyLinkedListInternal storage self
     ) private returns (bytes32 value) {
-        value = _prev(queue, 0);
-        _remove(queue, value);
+        value = _prev(self, 0);
+        _remove(self, value);
     }
 
     function _shift(
-        DoublyLinkedListInternal storage queue
+        DoublyLinkedListInternal storage self
     ) private returns (bytes32 value) {
-        value = _next(queue, 0);
-        _remove(queue, value);
+        value = _next(self, 0);
+        _remove(self, value);
     }
 
     function _unshift(
-        DoublyLinkedListInternal storage queue,
+        DoublyLinkedListInternal storage self,
         bytes32 value
     ) private returns (bool status) {
-        status = _insertBetween(queue, 0, _next(queue, 0), value);
+        status = _insertBetween(self, 0, _next(self, 0), value);
     }
 
     function _remove(
-        DoublyLinkedListInternal storage queue,
+        DoublyLinkedListInternal storage self,
         bytes32 value
     ) private returns (bool status) {
-        if (_contains(queue, value)) {
-            _link(queue, _prev(queue, value), _next(queue, value));
-            delete queue._desc[value];
-            delete queue._asc[value];
+        if (_contains(self, value)) {
+            _link(self, _prev(self, value), _next(self, value));
+            delete self._desc[value];
+            delete self._asc[value];
             status = true;
         }
     }
 
     function _link(
-        DoublyLinkedListInternal storage queue,
+        DoublyLinkedListInternal storage self,
         bytes32 prevValue,
         bytes32 nextValue
     ) private {
-        queue._asc[prevValue] = nextValue;
-        queue._desc[nextValue] = prevValue;
+        self._asc[prevValue] = nextValue;
+        self._desc[nextValue] = prevValue;
     }
 }
