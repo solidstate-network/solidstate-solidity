1e69c437ac27023634580d45db8a724addb8f2c7
Nick Barry <itsnickbarry@protonmail.ch>
2022-12-26 15:43:44 +0200

    test address linked list

--- test/data/DoublyLinkedList.ts
+++ test/data/DoublyLinkedList.ts
@@ -488,4 +488,480 @@ describe('DoublyLinkedList', async () => {
       });
     });
   });
+
+  describe('AddressList', async () => {
+    let instance: DoublyLinkedListAddressMock;
+    let deployer: SignerWithAddress;
+
+    beforeEach(async () => {
+      [deployer] = await ethers.getSigners();
+      instance = await new DoublyLinkedListAddressMock__factory(
+        deployer,
+      ).deploy();
+    });
+
+    describe('__internal', () => {
+      const zeroAddress = bnToAddress(ethers.constants.Zero);
+      const oneAddress = bnToAddress(ethers.constants.One);
+      const twoAddress = bnToAddress(ethers.constants.Two);
+      const threeAddress = bnToAddress(BigNumber.from('3'));
+
+      describe('#contains(address)', () => {
+        it('returns true if the value has been added', async () => {
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+
+          expect(await instance['contains(address)'](oneAddress)).to.be.true;
+          expect(await instance['contains(address)'](twoAddress)).to.be.true;
+        });
+
+        it('returns false if the value has not been added', async () => {
+          expect(await instance['contains(address)'](oneAddress)).to.be.false;
+          await instance['push(address)'](oneAddress);
+          expect(await instance['contains(address)'](twoAddress)).to.be.false;
+        });
+
+        it('returns false for zero value', async () => {
+          expect(await instance['contains(address)'](zeroAddress)).to.be.false;
+        });
+      });
+
+      describe('#prev(address)', () => {
+        it('returns the previous value in the list', async () => {
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+
+          expect(await instance['prev(address)'](twoAddress)).to.eq(oneAddress);
+        });
+
+        it('returns zero if the value is at the beginning of the list', async () => {
+          await instance['push(address)'](oneAddress);
+
+          expect(await instance['prev(address)'](oneAddress)).to.eq(
+            zeroAddress,
+          );
+        });
+
+        it('returns last value in list if input is zero', async () => {
+          expect(await instance['next(address)'](zeroAddress)).to.eq(
+            zeroAddress,
+          );
+
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+
+          expect(await instance['prev(address)'](zeroAddress)).to.eq(
+            twoAddress,
+          );
+        });
+
+        describe('reverts if', () => {
+          it('value is not contained in list', async () => {
+            await expect(
+              instance['prev(address)'](oneAddress),
+            ).to.be.revertedWithCustomError(
+              instance,
+              'DoublyLinkedList__InvalidValue',
+            );
+          });
+        });
+      });
+
+      describe('#next(address)', () => {
+        it('returns the next value in the list', async () => {
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+
+          expect(await instance['next(address)'](oneAddress)).to.eq(twoAddress);
+        });
+
+        it('returns zero if the value is at the end of the list', async () => {
+          await instance['push(address)'](oneAddress);
+
+          expect(await instance['next(address)'](oneAddress)).to.eq(
+            zeroAddress,
+          );
+        });
+
+        it('returns first value in list if input is zero', async () => {
+          expect(await instance['next(address)'](zeroAddress)).to.eq(
+            zeroAddress,
+          );
+
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+
+          expect(await instance['next(address)'](zeroAddress)).to.eq(
+            oneAddress,
+          );
+        });
+
+        describe('reverts if', () => {
+          it('value is not contained in list', async () => {
+            await expect(
+              instance['next(address)'](oneAddress),
+            ).to.be.revertedWithCustomError(
+              instance,
+              'DoublyLinkedList__InvalidValue',
+            );
+          });
+        });
+      });
+
+      describe('#insertBefore(address,address)', () => {
+        it('returns true if value is added to list', async () => {
+          expect(
+            await instance.callStatic['insertBefore(address,address)'](
+              zeroAddress,
+              oneAddress,
+            ),
+          ).to.be.true;
+        });
+
+        it('returns false if value is not added to list', async () => {
+          await instance['push(address)'](oneAddress);
+
+          expect(
+            await instance.callStatic['insertBefore(address,address)'](
+              zeroAddress,
+              oneAddress,
+            ),
+          ).to.be.false;
+        });
+
+        it('adds new value to list in position before existing value', async () => {
+          await instance['insertBefore(address,address)'](
+            zeroAddress,
+            oneAddress,
+          );
+
+          expect(await instance['next(address)'](oneAddress)).to.eq(
+            zeroAddress,
+          );
+
+          await instance['insertBefore(address,address)'](
+            oneAddress,
+            twoAddress,
+          );
+
+          expect(await instance['next(address)'](twoAddress)).to.eq(oneAddress);
+        });
+
+        describe('reverts if', () => {
+          it('new value is zero', async () => {
+            await expect(
+              instance['insertBefore(address,address)'](
+                zeroAddress,
+                zeroAddress,
+              ),
+            ).to.be.revertedWithCustomError(
+              instance,
+              'DoublyLinkedList__InvalidValue',
+            );
+          });
+
+          it('value is not contained in list', async () => {
+            await expect(
+              instance['insertBefore(address,address)'](oneAddress, twoAddress),
+            ).to.be.revertedWithCustomError(
+              instance,
+              'DoublyLinkedList__InvalidValue',
+            );
+          });
+        });
+      });
+
+      describe('#insertAfter(address,address)', () => {
+        it('returns true if value is added to list', async () => {
+          expect(
+            await instance.callStatic['insertAfter(address,address)'](
+              zeroAddress,
+              oneAddress,
+            ),
+          ).to.be.true;
+        });
+
+        it('returns false if value is not added to list', async () => {
+          await instance['push(address)'](oneAddress);
+
+          expect(
+            await instance.callStatic['insertAfter(address,address)'](
+              zeroAddress,
+              oneAddress,
+            ),
+          ).to.be.false;
+        });
+
+        it('adds new value to list in position before existing value', async () => {
+          await instance['insertAfter(address,address)'](
+            zeroAddress,
+            oneAddress,
+          );
+
+          expect(await instance['prev(address)'](oneAddress)).to.eq(
+            zeroAddress,
+          );
+
+          await instance['insertAfter(address,address)'](
+            oneAddress,
+            twoAddress,
+          );
+
+          expect(await instance['prev(address)'](twoAddress)).to.eq(oneAddress);
+        });
+
+        describe('reverts if', () => {
+          it('new value is zero', async () => {
+            await expect(
+              instance['insertAfter(address,address)'](
+                zeroAddress,
+                zeroAddress,
+              ),
+            ).to.be.revertedWithCustomError(
+              instance,
+              'DoublyLinkedList__InvalidValue',
+            );
+          });
+
+          it('value is not contained in list', async () => {
+            await expect(
+              instance['insertAfter(address,address)'](oneAddress, twoAddress),
+            ).to.be.revertedWithCustomError(
+              instance,
+              'DoublyLinkedList__InvalidValue',
+            );
+          });
+        });
+      });
+
+      describe('#push(address)', () => {
+        it('returns true if value is added to list', async () => {
+          expect(await instance.callStatic['push(address)'](oneAddress)).to.be
+            .true;
+        });
+
+        it('returns false if value is not added to list', async () => {
+          await instance['push(address)'](oneAddress);
+
+          expect(await instance.callStatic['push(address)'](oneAddress)).to.be
+            .false;
+        });
+
+        it('adds new value to end of list', async () => {
+          await instance['push(address)'](oneAddress);
+
+          expect(await instance['next(address)'](oneAddress)).to.eq(
+            zeroAddress,
+          );
+
+          await instance['push(address)'](twoAddress);
+
+          expect(await instance['next(address)'](oneAddress)).to.eq(twoAddress);
+        });
+
+        describe('reverts if', () => {
+          it('new value is zero', async () => {
+            await expect(
+              instance['push(address)'](zeroAddress),
+            ).to.be.revertedWithCustomError(
+              instance,
+              'DoublyLinkedList__InvalidValue',
+            );
+          });
+        });
+      });
+
+      describe('#pop()', () => {
+        it('returns last value in list', async () => {
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+
+          expect(await instance.callStatic['pop()']()).to.eq(twoAddress);
+        });
+
+        it('returns zero if list is empty', async () => {
+          expect(await instance.callStatic['pop()']()).to.eq(zeroAddress);
+        });
+
+        it('removes last value from list', async () => {
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+
+          await instance['pop()']();
+
+          expect(await instance['contains(address)'](twoAddress)).to.be.false;
+
+          expect(await instance['prev(address)'](zeroAddress)).to.eq(
+            oneAddress,
+          );
+
+          expect(await instance['next(address)'](oneAddress)).to.eq(
+            zeroAddress,
+          );
+        });
+      });
+
+      describe('#shift()', () => {
+        it('returns first value in list', async () => {
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+
+          expect(await instance.callStatic['shift()']()).to.eq(oneAddress);
+        });
+
+        it('returns zero if list is empty', async () => {
+          expect(await instance.callStatic['shift()']()).to.eq(zeroAddress);
+        });
+
+        it('removes first value from list', async () => {
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+
+          await instance['shift()']();
+
+          expect(await instance['contains(address)'](oneAddress)).to.be.false;
+
+          expect(await instance['next(address)'](zeroAddress)).to.eq(
+            twoAddress,
+          );
+
+          expect(await instance['prev(address)'](twoAddress)).to.eq(
+            zeroAddress,
+          );
+        });
+      });
+
+      describe('#unshift(address)', () => {
+        it('returns true if value is added to list', async () => {
+          expect(await instance.callStatic['unshift(address)'](oneAddress)).to
+            .be.true;
+        });
+
+        it('returns false if value is not added to list', async () => {
+          await instance['unshift(address)'](oneAddress);
+
+          expect(await instance.callStatic['unshift(address)'](oneAddress)).to
+            .be.false;
+        });
+
+        it('adds new value to beginning of list', async () => {
+          await instance['unshift(address)'](oneAddress);
+
+          expect(await instance['prev(address)'](oneAddress)).to.eq(
+            zeroAddress,
+          );
+
+          await instance['unshift(address)'](twoAddress);
+
+          expect(await instance['prev(address)'](oneAddress)).to.eq(twoAddress);
+        });
+
+        describe('reverts if', () => {
+          it('new value is zero', async () => {
+            await expect(
+              instance['unshift(address)'](zeroAddress),
+            ).to.be.revertedWithCustomError(
+              instance,
+              'DoublyLinkedList__InvalidValue',
+            );
+          });
+        });
+      });
+
+      describe('#remove(address)', () => {
+        it('returns true if value is removed from list', async () => {
+          await instance['push(address)'](oneAddress);
+
+          expect(await instance.callStatic['remove(address)'](oneAddress)).to.be
+            .true;
+        });
+
+        it('returns false if value is not removed from list', async () => {
+          expect(await instance.callStatic['remove(address)'](oneAddress)).to.be
+            .false;
+        });
+
+        it('removes value from list', async () => {
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+          await instance['push(address)'](threeAddress);
+
+          await instance['remove(address)'](twoAddress);
+
+          expect(await instance['contains(address)'](twoAddress)).to.be.false;
+
+          expect(await instance['next(address)'](oneAddress)).to.eq(
+            threeAddress,
+          );
+
+          expect(await instance['prev(address)'](threeAddress)).to.eq(
+            oneAddress,
+          );
+        });
+      });
+
+      describe('#replace(address,address)', () => {
+        it('returns true if value is replaced', async () => {
+          await instance['push(address)'](oneAddress);
+
+          expect(
+            await instance.callStatic['replace(address,address)'](
+              oneAddress,
+              twoAddress,
+            ),
+          ).to.be.true;
+        });
+
+        it('returns false if value is not replaced', async () => {
+          expect(
+            await instance.callStatic['replace(address,address)'](
+              oneAddress,
+              twoAddress,
+            ),
+          ).to.be.false;
+
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+
+          expect(
+            await instance.callStatic['replace(address,address)'](
+              oneAddress,
+              twoAddress,
+            ),
+          ).to.be.false;
+        });
+
+        it('replaces existing value with new value', async () => {
+          const newValue = bnToAddress(BigNumber.from('4'));
+
+          await instance['push(address)'](oneAddress);
+          await instance['push(address)'](twoAddress);
+          await instance['push(address)'](threeAddress);
+
+          expect(await instance['contains(address)'](newValue)).to.be.false;
+
+          await instance['replace(address,address)'](twoAddress, newValue);
+
+          expect(await instance['contains(address)'](twoAddress)).to.be.false;
+          expect(await instance['contains(address)'](newValue)).to.be.true;
+
+          expect(await instance['next(address)'](oneAddress)).to.eq(newValue);
+          expect(await instance['prev(address)'](newValue)).to.eq(oneAddress);
+          expect(await instance['next(address)'](newValue)).to.eq(threeAddress);
+          expect(await instance['prev(address)'](threeAddress)).to.eq(newValue);
+        });
+
+        describe('reverts if', () => {
+          it('new value is zero', async () => {
+            await expect(
+              instance['replace(address,address)'](oneAddress, zeroAddress),
+            ).to.be.revertedWithCustomError(
+              instance,
+              'DoublyLinkedList__InvalidValue',
+            );
+          });
+        });
+      });
+    });
+  });
 });
