ebb3333e6990a825dd52cc30cc9ecb8058941f61
Nick Barry <itsnickbarry@protonmail.ch>
2023-02-27 19:23:25 -0600

    add missing imports to IERC4626BaseInternal

--- contracts/token/ERC4626/base/IERC4626BaseInternal.sol
+++ contracts/token/ERC4626/base/IERC4626BaseInternal.sol
@@ -3,11 +3,17 @@
 pragma solidity ^0.8.8;
 
 import { IERC4626Internal } from '../../../interfaces/IERC4626Internal.sol';
+import { IERC20BaseInternal } from '../../ERC20/base/IERC20BaseInternal.sol';
+import { IERC20MetadataInternal } from '../../ERC20/metadata/IERC20MetadataInternal.sol';
 
 /**
  * @title ERC4626 base interface
  */
-interface IERC4626BaseInternal is IERC4626Internal {
+interface IERC4626BaseInternal is
+    IERC4626Internal,
+    IERC20BaseInternal,
+    IERC20MetadataInternal
+{
     error ERC4626Base__MaximumAmountExceeded();
     error ERC4626Base__AllowanceExceeded();
 }
