97419ee71e484eb24cab859f7deb53fb07653515
Nick Barry <itsnickbarry@protonmail.ch>
2023-05-23 21:39:08 +0300

    remove ERC20 reference

--- contracts/cryptography/EIP712.sol
+++ contracts/cryptography/EIP712.sol
@@ -16,7 +16,7 @@ library EIP712 {
      * @notice calculate unique EIP-712 domain separator
      * @dev name and version inputs are hashed as required by EIP-712 because they are of dynamic-length types
      * @dev implementation of EIP712Domain struct type excludes the optional salt parameter
-     * @param nameHash hash of ERC20Metadata token name
+     * @param nameHash hash of human-readable signing domain name
      * @param versionHash hash of signing domain version
      * @return domainSeparator domain separator
      */
