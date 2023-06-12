631ce7765c80443c4fa84c769138bd629ba10c2d
Nick Barry <itsnickbarry@protonmail.ch>
2023-06-13 01:18:00 +0300

    temporarily fix bn conversion functions

--- lib/bn_conversion.ts
+++ lib/bn_conversion.ts
@@ -1,14 +1,16 @@
 import { BigNumber, BigNumberish } from 'ethers';
 import { ethers } from 'hardhat';
 
+// TODO: these no longer use bn and should be renamed or removed
+
 export function bnToAddress(bn: BigNumber | BigNumberish) {
-  return ethers.getAddress(ethers.hexZeroPad(ethers.hexlify(bn), 20));
+  return ethers.getAddress(ethers.zeroPadValue(ethers.toBeHex(bn), 20));
 }
 
 export function bnToBytes32(bn: BigNumber | BigNumberish) {
-  return ethers.hexZeroPad(ethers.hexlify(bn), 32);
+  return ethers.zeroPadValue(ethers.toBeHex(bn), 32);
 }
 
 export function bytes32ToNumber(bytes32: string | BigNumber | BigNumberish) {
-  return BigNumber.from(bytes32).toNumber();
+  return BigInt(bytes32);
 }
