2629491409e0cc8e3d458b548a50b5629b10e8c5
Nick Barry <itsnickbarry@protonmail.ch>
2023-06-22 02:06:48 +0300

    remove strict typing from bn conversion utils

--- lib/bn_conversion.ts
+++ lib/bn_conversion.ts
@@ -2,14 +2,14 @@ import { ethers } from 'hardhat';
 
 // TODO: these no longer use bn and should be renamed or removed
 
-export function bnToAddress(bn: BigInt) {
+export function bnToAddress(bn: any) {
   return ethers.getAddress(ethers.zeroPadValue(ethers.toBeHex(bn), 20));
 }
 
-export function bnToBytes32(bn: BigInt) {
+export function bnToBytes32(bn: any) {
   return ethers.zeroPadValue(ethers.toBeHex(bn), 32);
 }
 
-export function bytes32ToNumber(bytes32: string | BigInt) {
+export function bytes32ToNumber(bytes32: any) {
   return BigInt(bytes32);
 }
