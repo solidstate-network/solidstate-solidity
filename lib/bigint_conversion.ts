caf860bdba82d3d2d876b908e756c54e44ed69c7
Nick Barry <itsnickbarry@protonmail.ch>
2023-07-29 04:05:49 +0300

    remove todo

--- lib/bigint_conversion.ts
+++ lib/bigint_conversion.ts
@@ -1,7 +1,5 @@
 import { ethers } from 'hardhat';
 
-// TODO: these no longer use bn and should be renamed or removed
-
 export function bigintToAddress(bn: any) {
   return ethers.getAddress(ethers.zeroPadValue(ethers.toBeHex(bn), 20));
 }
