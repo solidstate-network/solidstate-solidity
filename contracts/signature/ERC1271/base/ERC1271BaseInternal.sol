0bb380571d1321bcf411ff59bb4fec91d5a13b5e
Nick Barry <itsnickbarry@protonmail.ch>
2024-01-30 23:31:26 +0200

    move ERC1271 files to dedicated directory

--- contracts/signature/ERC1271/base/ERC1271BaseInternal.sol
+++ contracts/signature/ERC1271/base/ERC1271BaseInternal.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-import { IERC1271 } from '../../interfaces/IERC1271.sol';
+import { IERC1271 } from '../../../interfaces/IERC1271.sol';
 
 /**
  * @title ERC1271Base internal functions
