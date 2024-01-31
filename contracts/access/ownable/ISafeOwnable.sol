cbb3e154d08f2c64b02dd57bae355febe7552e1a
Nick Barry <itsnickbarry@protonmail.ch>
2024-01-31 16:21:22 +0200

    update minimum pragma version to 0.8.20

--- contracts/access/ownable/ISafeOwnable.sol
+++ contracts/access/ownable/ISafeOwnable.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 import { IOwnable } from './IOwnable.sol';
 import { ISafeOwnableInternal } from './ISafeOwnableInternal.sol';
