083df990cee8fd6d38a563ed898d167f48af2fab
Nick Barry <itsnickbarry@protonmail.ch>
2024-05-14 01:29:59 +0400

    remove unused import

--- spec/token/ERC1404/SolidStateERC1404.behavior.ts
+++ spec/token/ERC1404/SolidStateERC1404.behavior.ts
@@ -8,7 +8,6 @@ import {
 } from './ERC1404Base.behavior';
 import { describeFilter } from '@solidstate/library';
 import { ISolidStateERC1404 } from '@solidstate/typechain-types';
-import { ContractTransaction } from 'ethers';
 
 export interface SolidStateERC1404BehaviorArgs
   extends SolidStateERC20BehaviorArgs,
