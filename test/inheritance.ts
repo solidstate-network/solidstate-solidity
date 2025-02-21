5647764cdfb4a9fa23fcead541b6a73dcd8c75d1
Nick Barry <itsnickbarry@protonmail.ch>
2025-02-21 13:09:29 -0600

    remove path from fully qualified names before running inheritance tests

--- test/inheritance.ts
+++ test/inheritance.ts
@@ -11,21 +11,23 @@ const EXTERNAL_CONTRACT =
   /\b(([I][a-z])|([A-HJ-Z]))\w*(?<!Internal|Storage|Mock)$/;
 
 describe('Inheritance Graph', () => {
-  let allFullyQualifiedNames: string[];
+  let allEntityNames: string[];
   const ancestors: { [key: string]: string[] } = {};
   const directAncestors: { [key: string]: string[] } = {};
 
   before(async () => {
-    allFullyQualifiedNames = await hre.artifacts.getAllFullyQualifiedNames();
+    const allFullyQualifiedNames =
+      await hre.artifacts.getAllFullyQualifiedNames();
+    allEntityNames = allFullyQualifiedNames.map((name) => name.split(':')[1]);
 
     for (const name of allFullyQualifiedNames) {
       const [path, entity] = name.split(':');
       // TODO: sources are read directly from disk and may not match compiled artifacts
       // track full inheritance trees via surya
-      ancestors[name] = (await surya.dependencies([path], entity)).slice(1);
+      ancestors[entity] = (await surya.dependencies([path], entity)).slice(1);
       // read direct inheritance trees from source
       const file = fs.readFileSync(path).toString();
-      directAncestors[name] =
+      directAncestors[entity] =
         file
           .match(/(contract|interface)\s+(\w+)\s+is((\s+\w+,?)+)\s+{/)?.[3]
           .trim()
@@ -37,9 +39,7 @@ describe('Inheritance Graph', () => {
     let names: string[];
 
     before(async () => {
-      names = allFullyQualifiedNames.filter((name) =>
-        INTERNAL_INTERFACE.test(name),
-      );
+      names = allEntityNames.filter((name) => INTERNAL_INTERFACE.test(name));
     });
 
     it('do not inherit from contracts or external interfaces', async () => {
@@ -67,9 +67,7 @@ describe('Inheritance Graph', () => {
     let names: string[];
 
     before(async () => {
-      names = allFullyQualifiedNames.filter((name) =>
-        EXTERNAL_INTERFACE.test(name),
-      );
+      names = allEntityNames.filter((name) => EXTERNAL_INTERFACE.test(name));
     });
 
     it('do not inherit from contracts', async () => {
@@ -84,14 +82,13 @@ describe('Inheritance Graph', () => {
     it('do not directly inherit from unrelated internal interfaces', async () => {
       for (const name of names) {
         for (const ancestor of directAncestors[name]) {
-          const [, entity] = name.split(':');
-          const internalInterfaceName = `${entity}Internal`;
+          const internalInterfaceName = `${name}Internal`;
 
           if (ancestor === internalInterfaceName) continue;
 
           expect(INTERNAL_INTERFACE.test(ancestor)).to.eq(
             false,
-            `Invalid direct ancestor for ${entity}: ${ancestor}`,
+            `Invalid direct ancestor for ${name}: ${ancestor}`,
           );
         }
       }
@@ -99,24 +96,22 @@ describe('Inheritance Graph', () => {
 
     it('directly inherit from corresponding internal interfaces', async () => {
       for (const name of names) {
-        const [, entity] = name.split(':');
-        const internalInterfaceName = `${entity}Internal`;
+        const internalInterfaceName = `${name}Internal`;
 
         expect(directAncestors[name]).to.include(
           internalInterfaceName,
-          `Missing ancestor for ${entity}`,
+          `Missing ancestor for ${name}`,
         );
       }
     });
 
     it('inherit in correct order', async () => {
       for (const name of names) {
-        const [, entity] = name.split(':');
-        const internalInterfaceName = `${entity}Internal`;
+        const internalInterfaceName = `${name}Internal`;
 
         expect(ancestors[name].indexOf(internalInterfaceName)).to.eq(
           ancestors[name].length - 1,
-          `First inherited ancestor for ${entity} should be ${internalInterfaceName}`,
+          `First inherited ancestor for ${name} should be ${internalInterfaceName}`,
         );
       }
     });
@@ -136,9 +131,7 @@ describe('Inheritance Graph', () => {
     let names: string[];
 
     before(async () => {
-      names = allFullyQualifiedNames.filter((name) =>
-        INTERNAL_CONTRACT.test(name),
-      );
+      names = allEntityNames.filter((name) => INTERNAL_CONTRACT.test(name));
     });
 
     it('do not inherit from external contracts', async () => {
@@ -152,14 +145,13 @@ describe('Inheritance Graph', () => {
     it('do not directly inherit from unrelated internal interfaces', async () => {
       for (const name of names) {
         for (const ancestor of directAncestors[name]) {
-          const [, entity] = name.split(':');
-          const internalInterfaceName = `I${entity}`;
+          const internalInterfaceName = `I${name}`;
 
           if (ancestor === internalInterfaceName) continue;
 
           expect(INTERNAL_INTERFACE.test(ancestor)).to.eq(
             false,
-            `Invalid direct ancestor for ${entity}: ${ancestor}`,
+            `Invalid direct ancestor for ${name}: ${ancestor}`,
           );
         }
       }
@@ -167,24 +159,22 @@ describe('Inheritance Graph', () => {
 
     it('directly inherit from corresponding internal interfaces', async () => {
       for (const name of names) {
-        const [, entity] = name.split(':');
-        const internalInterfaceName = `I${entity}`;
+        const internalInterfaceName = `I${name}`;
 
         expect(directAncestors[name]).to.include(
           internalInterfaceName,
-          `Missing ancestor for ${entity}`,
+          `Missing ancestor for ${name}`,
         );
       }
     });
 
     it('inherit in correct order', async () => {
       for (const name of names) {
-        const [, entity] = name.split(':');
-        const internalInterfaceName = `I${entity}`;
+        const internalInterfaceName = `I${name}`;
 
         expect(ancestors[name].indexOf(internalInterfaceName)).to.eq(
           ancestors[name].length - 1,
-          `First inherited ancestor for ${entity} should be ${internalInterfaceName}`,
+          `First inherited ancestor for ${name} should be ${internalInterfaceName}`,
         );
       }
     });
@@ -204,9 +194,7 @@ describe('Inheritance Graph', () => {
     let names: string[];
 
     before(async () => {
-      names = allFullyQualifiedNames.filter((name) =>
-        EXTERNAL_CONTRACT.test(name),
-      );
+      names = allEntityNames.filter((name) => EXTERNAL_CONTRACT.test(name));
 
       // TODO: skip libraries dynamically
       names = names.filter(
@@ -230,18 +218,16 @@ describe('Inheritance Graph', () => {
             'SafeERC20',
             'StorageUtils',
             'UintUtils',
-          ].includes(name.split(':')[1]),
+          ].includes(name),
       );
     });
 
     it('do not directly inherit from internal interfaces', async () => {
       for (const name of names) {
         for (const ancestor of directAncestors[name]) {
-          const [, entity] = name.split(':');
-
           expect(INTERNAL_INTERFACE.test(ancestor)).to.eq(
             false,
-            `Invalid direct ancestor for ${entity}: ${ancestor}`,
+            `Invalid direct ancestor for ${name}: ${ancestor}`,
           );
         }
       }
@@ -250,14 +236,13 @@ describe('Inheritance Graph', () => {
     it('do not directly inherit from unrelated external interfaces', async () => {
       for (const name of names) {
         for (const ancestor of directAncestors[name]) {
-          const [, entity] = name.split(':');
-          const externalInterfaceName = `I${entity}`;
+          const externalInterfaceName = `I${name}`;
 
           if (ancestor === externalInterfaceName) continue;
 
           expect(EXTERNAL_INTERFACE.test(ancestor)).to.eq(
             false,
-            `Invalid direct ancestor for ${entity}: ${ancestor}`,
+            `Invalid direct ancestor for ${name}: ${ancestor}`,
           );
         }
       }
@@ -266,14 +251,13 @@ describe('Inheritance Graph', () => {
     it('do not directly inherit from unrelated internal contracts', async () => {
       for (const name of names) {
         for (const ancestor of directAncestors[name]) {
-          const [, entity] = name.split(':');
-          const internalContractName = `${entity}Internal`;
+          const internalContractName = `${name}Internal`;
 
           if (ancestor === internalContractName) continue;
 
           expect(INTERNAL_CONTRACT.test(ancestor)).to.eq(
             false,
-            `Invalid direct ancestor for ${entity}: ${ancestor}`,
+            `Invalid direct ancestor for ${name}: ${ancestor}`,
           );
         }
       }
@@ -281,35 +265,33 @@ describe('Inheritance Graph', () => {
 
     it('directly inherit from corresponding internal contracts and external interfaces', async () => {
       for (const name of names) {
-        const [, entity] = name.split(':');
-        const internalContractName = `${entity}Internal`;
-        const externalInterfaceName = `I${entity}`;
+        const internalContractName = `${name}Internal`;
+        const externalInterfaceName = `I${name}`;
 
         expect(directAncestors[name]).to.include(
           internalContractName,
-          `Missing ancestor for ${entity}`,
+          `Missing ancestor for ${name}`,
         );
 
         expect(directAncestors[name]).to.include(
           externalInterfaceName,
-          `Missing ancestor for ${entity}`,
+          `Missing ancestor for ${name}`,
         );
       }
     });
 
     it('inherit in correct order', async () => {
       for (const name of names) {
-        const [, entity] = name.split(':');
-        const internalContractName = `${entity}Internal`;
-        const externalInterfaceName = `I${entity}`;
+        const internalContractName = `${name}Internal`;
+        const externalInterfaceName = `I${name}`;
 
         expect(ancestors[name].indexOf(internalContractName)).to.eq(
           ancestors[name].length - 2,
-          `First inherited ancestor for ${entity} should be ${internalContractName}`,
+          `First inherited ancestor for ${name} should be ${internalContractName}`,
         );
         expect(ancestors[name].indexOf(externalInterfaceName)).to.eq(
           ancestors[name].length - 1,
-          `Second inherited ancestor for ${entity} should be ${externalInterfaceName}`,
+          `Second inherited ancestor for ${name} should be ${externalInterfaceName}`,
         );
       }
     });
@@ -317,7 +299,7 @@ describe('Inheritance Graph', () => {
 
   describe('Uncategorized Entities', async () => {
     it('do not exist', async () => {
-      for (const name of allFullyQualifiedNames) {
+      for (const name of allEntityNames) {
         // TODO: account for libraries
         expect(
           INTERNAL_INTERFACE.test(name) ||
