98a9b13ffb9660c36ceb13dc3c008ed5e923a959
Nick Barry <itsnickbarry@protonmail.ch>
2025-02-22 12:07:07 -0600

    add missing pragma statement

--- contracts/proxy/beacon/diamond/DiamondBeaconProxyInternal.sol
+++ contracts/proxy/beacon/diamond/DiamondBeaconProxyInternal.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.20;
+
 import { BeaconProxyInternal } from '../BeaconProxyInternal.sol';
 import { IDiamondBeacon } from './IDiamondBeacon.sol';
 import { IDiamondBeaconProxyInternal } from './IDiamondBeaconProxyInternal.sol';
