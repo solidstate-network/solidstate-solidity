73976c8c8a131d019e7e87814adb61633effe25c
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-05 23:22:14 -0600

    update seedToStorageSlot helper function

--- lib/storage_layout.ts
+++ lib/storage_layout.ts
@@ -1,5 +1,18 @@
 import { ethers } from 'hardhat';
 
-export function seedToStorageSlot(seed: string) {
-  return ethers.solidityPackedKeccak256(['string'], [seed]);
+// TODO: rename to indicate that formula is from EIP-7201
+export function seedToStorageSlot(seed: string): string {
+  return (
+    '0x' +
+    (
+      (BigInt(
+        ethers.solidityPackedKeccak256(
+          ['uint256'],
+          [BigInt(ethers.solidityPackedKeccak256(['string'], [seed])) - 1n],
+        ),
+      ) /
+        256n) *
+      256n
+    ).toString(16)
+  );
 }
