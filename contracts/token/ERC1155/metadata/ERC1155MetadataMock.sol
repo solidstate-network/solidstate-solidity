32f3127113af09b836654f9928b57ca6469c2105
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-07 11:27:03 -0600

    apply layers pattern to ERC1155Metadata

--- contracts/token/ERC1155/metadata/ERC1155MetadataMock.sol
+++ contracts/token/ERC1155/metadata/ERC1155MetadataMock.sol
@@ -2,7 +2,8 @@
 
 pragma solidity ^0.8.20;
 
-import { ERC1155Metadata, ERC1155MetadataStorage } from './ERC1155Metadata.sol';
+import { ERC1155Metadata } from './ERC1155Metadata.sol';
+import { ERC1155MetadataStorage } from './ERC1155MetadataStorage.sol';
 import { ERC165Base } from '../../../introspection/ERC165/base/ERC165Base.sol';
 
 contract ERC1155MetadataMock is ERC1155Metadata, ERC165Base {
