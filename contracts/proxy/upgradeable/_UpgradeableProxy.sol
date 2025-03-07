f80cf2d725523a78841f8f50f63d7ef1db5a2b1c
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-07 17:56:57 -0600

    merge UpgradeableProxyOwnable into UpgradeableProxy

--- contracts/proxy/upgradeable/_UpgradeableProxy.sol
+++ contracts/proxy/upgradeable/_UpgradeableProxy.sol
@@ -2,8 +2,17 @@
 
 pragma solidity ^0.8.20;
 
+import { _Ownable } from '../../access/ownable/_Ownable.sol';
 import { _Proxy } from '../_Proxy.sol';
 import { _IUpgradeableProxy } from './_IUpgradeableProxy.sol';
-import { UpgradeableProxyStorage } from './UpgradeableProxyStorage.sol';
 
-abstract contract _UpgradeableProxy is _IUpgradeableProxy, _Proxy {}
+abstract contract _UpgradeableProxy is _IUpgradeableProxy, _Proxy, _Ownable {
+    /**
+     * TODO: standardize use of externally accessible functions with "External" suffix
+     */
+    function _setImplementationExternal(
+        address implementation
+    ) internal virtual onlyOwner {
+        _setImplementation(implementation);
+    }
+}
