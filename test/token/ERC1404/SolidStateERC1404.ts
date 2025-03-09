834f54154912aef58f39ea19efc95a788b5f8008
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-08 18:25:21 -0600

    remove ERC1404 mocks

--- test/token/ERC1404/SolidStateERC1404.ts
+++ test/token/ERC1404/SolidStateERC1404.ts
@@ -1,7 +1,7 @@
 import { describeBehaviorOfSolidStateERC1404 } from '@solidstate/spec';
 import {
-  SolidStateERC1404Mock,
-  SolidStateERC1404Mock__factory,
+  __hh_exposed_SolidStateERC1404,
+  __hh_exposed_SolidStateERC1404__factory,
 } from '@solidstate/typechain-types';
 import { ethers } from 'hardhat';
 
@@ -10,40 +10,26 @@ let restrictions = [
   { code: 3n, message: 'three' },
 ];
 
-const invalidTransfer = {
-  sender: ethers.ZeroAddress,
-  receiver: ethers.ZeroAddress,
-  amount: 1n,
-  code: 3n,
-};
-
 describe('SolidStateERC1404', () => {
-  let instance: SolidStateERC1404Mock;
+  let instance: __hh_exposed_SolidStateERC1404;
 
   beforeEach(async () => {
     const [deployer] = await ethers.getSigners();
-    instance = await new SolidStateERC1404Mock__factory(deployer).deploy();
-    await instance.setRestrictions(
+    instance = await new __hh_exposed_SolidStateERC1404__factory(
+      deployer,
+    ).deploy();
+    await instance.__hh_exposed__setRestrictions(
       restrictions.map((r) => r.code),
       restrictions.map((r) => r.message),
-    );
-    invalidTransfer.sender = await deployer.getAddress();
-    invalidTransfer.receiver = await deployer.getAddress();
-    await instance.setInvalidTransfer(
-      invalidTransfer.sender,
-      invalidTransfer.receiver,
-      invalidTransfer.amount,
-      invalidTransfer.code,
     );
   });
 
   describeBehaviorOfSolidStateERC1404(async () => instance, {
-    mint: (recipient, amount) => instance.__mint(recipient, amount),
-    burn: (recipient, amount) => instance.__burn(recipient, amount),
+    mint: (recipient, amount) => instance.__hh_exposed__mint(recipient, amount),
+    burn: (recipient, amount) => instance.__hh_exposed__burn(recipient, amount),
     allowance: (holder, spender) =>
       instance.allowance.staticCall(holder, spender),
     restrictions,
-    invalidTransfer,
     name: '',
     symbol: '',
     decimals: 0n,
