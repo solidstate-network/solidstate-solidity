c9944aa5d52f3c66ea4499b3fe001855dc63dca7
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-08 21:42:24 -0600

    integrate ERC5267 into ERC20Permit

--- contracts/interfaces/IERC5267.sol
+++ contracts/interfaces/IERC5267.sol
@@ -9,6 +9,16 @@ import { _IERC5267 } from './_IERC5267.sol';
  * @dev see https://eips.ethereum.org/EIPS/eip-5267.
  */
 interface IERC5267 is _IERC5267 {
+    /**
+     * @notice TODO
+     * @return fields
+     * @return name
+     * @return version
+     * @return chainId
+     * @return verifyingContract
+     * @return salt
+     * @return extensions
+     */
     function eip712Domain()
         external
         view
