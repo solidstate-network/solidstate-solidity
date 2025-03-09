5c141d1c17111335462b050fb585a6913de9fa7f
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-08 22:31:51 -0600

    include ERC1155Base in ERC1155Enumerable

--- contracts/token/ERC1155/enumerable/ERC1155Enumerable.sol
+++ contracts/token/ERC1155/enumerable/ERC1155Enumerable.sol
@@ -2,13 +2,19 @@
 
 pragma solidity ^0.8.20;
 
+import { ERC1155Base } from '../base/ERC1155Base.sol';
+import { _ERC1155Base } from '../base/_ERC1155Base.sol';
 import { IERC1155Enumerable } from './IERC1155Enumerable.sol';
 import { _ERC1155Enumerable } from './_ERC1155Enumerable.sol';
 
 /**
  * @title ERC1155 implementation including enumerable and aggregate functions
  */
-abstract contract ERC1155Enumerable is IERC1155Enumerable, _ERC1155Enumerable {
+abstract contract ERC1155Enumerable is
+    IERC1155Enumerable,
+    _ERC1155Enumerable,
+    ERC1155Base
+{
     /**
      * @inheritdoc IERC1155Enumerable
      */
@@ -39,5 +45,19 @@ abstract contract ERC1155Enumerable is IERC1155Enumerable, _ERC1155Enumerable {
         address account
     ) external view returns (uint256[] memory) {
         return _tokensByAccount(account);
+    }
+
+    /**
+     * @inheritdoc _ERC1155Enumerable
+     */
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override(_ERC1155Base, _ERC1155Enumerable) {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
