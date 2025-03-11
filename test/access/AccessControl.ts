d1ef271f32d5cf360a45d2bd0dafa3bbb11fdfb5
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-10 21:51:38 -0600

    test AccessControl onlyRole modifier

--- test/access/AccessControl.ts
+++ test/access/AccessControl.ts
@@ -24,7 +24,11 @@ describe('AccessControl', () => {
   beforeEach(async () => {
     instance = await new $AccessControl__factory(admin).deploy();
 
-    await instance.$_setRole(ethers.ZeroHash, await admin.getAddress(), true);
+    await instance.$_setRole(
+      DEFAULT_ADMIN_ROLE,
+      await admin.getAddress(),
+      true,
+    );
   });
 
   describeBehaviorOfAccessControl({
@@ -33,130 +37,156 @@ describe('AccessControl', () => {
     getNonAdmin: async () => nonAdmin,
   });
 
-  // TODO: test modifiers
+  describe('__internal', () => {
+    describe('onlyRole(bytes32) modifier', () => {
+      it('does not revert if sender has role', async () => {
+        await expect(instance.connect(admin).$onlyRole(DEFAULT_ADMIN_ROLE)).not
+          .to.be.reverted;
+      });
 
-  describe('#_checkRole(bytes32)', () => {
-    it('does not revert if sender has role', async () => {
-      await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
-
-      await expect(
-        instance.connect(nonAdmin)['$_checkRole(bytes32)'].staticCall(ROLE),
-      ).not.to.be.reverted;
-    });
-
-    describe('reverts if', () => {
-      it('sender does not have role', async () => {
-        await expect(
-          instance.connect(nonAdmin)['$_checkRole(bytes32)'].staticCall(ROLE),
-        ).to.revertedWith(
-          `AccessControl: account ${nonAdmin.address.toLowerCase()} is missing role ${ROLE}`,
-        );
+      describe('reverts if', () => {
+        it('sender does not have role', async () => {
+          await expect(
+            instance.connect(nonAdmin).$onlyRole(DEFAULT_ADMIN_ROLE),
+          ).to.be.revertedWith(
+            `AccessControl: account ${nonAdmin.address.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
+          );
+        });
       });
     });
-  });
 
-  describe('#_checkRole(bytes32,address)', () => {
-    it('does not revert if given account has role', async () => {
-      await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
+    describe('#_checkRole(bytes32)', () => {
+      it('does not revert if sender has role', async () => {
+        await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
 
-      await expect(
-        instance['$_checkRole(bytes32,address)'].staticCall(
-          ROLE,
-          nonAdmin.address,
-        ),
-      ).not.to.be.reverted;
+        await expect(
+          instance.connect(nonAdmin)['$_checkRole(bytes32)'].staticCall(ROLE),
+        ).not.to.be.reverted;
+      });
+
+      describe('reverts if', () => {
+        it('sender does not have role', async () => {
+          await expect(
+            instance.connect(nonAdmin)['$_checkRole(bytes32)'].staticCall(ROLE),
+          ).to.revertedWith(
+            `AccessControl: account ${nonAdmin.address.toLowerCase()} is missing role ${ROLE}`,
+          );
+        });
+      });
     });
 
-    describe('reverts if', () => {
-      it('given account does not have role', async () => {
+    describe('#_checkRole(bytes32,address)', () => {
+      it('does not revert if given account has role', async () => {
+        await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
+
         await expect(
           instance['$_checkRole(bytes32,address)'].staticCall(
             ROLE,
             nonAdmin.address,
           ),
-        ).to.revertedWith(
-          `AccessControl: account ${nonAdmin.address.toLowerCase()} is missing role ${ROLE}`,
-        );
+        ).not.to.be.reverted;
+      });
+
+      describe('reverts if', () => {
+        it('given account does not have role', async () => {
+          await expect(
+            instance['$_checkRole(bytes32,address)'].staticCall(
+              ROLE,
+              nonAdmin.address,
+            ),
+          ).to.revertedWith(
+            `AccessControl: account ${nonAdmin.address.toLowerCase()} is missing role ${ROLE}`,
+          );
+        });
       });
     });
-  });
 
-  describe('#_setRoleAdmin(bytes32,bytes32)', () => {
-    it('updates role admin', async () => {
-      const newAdminRole = ethers.solidityPackedKeccak256(
-        ['string'],
-        ['NEW_ADMIN_ROLE'],
-      );
+    describe('#_setRoleAdmin(bytes32,bytes32)', () => {
+      it('updates role admin', async () => {
+        const newAdminRole = ethers.solidityPackedKeccak256(
+          ['string'],
+          ['NEW_ADMIN_ROLE'],
+        );
 
-      await instance.$_setRoleAdmin(ROLE, newAdminRole);
+        await instance.$_setRoleAdmin(ROLE, newAdminRole);
 
-      expect(await instance.getRoleAdmin.staticCall(ROLE)).to.equal(
-        newAdminRole,
-      );
+        expect(await instance.getRoleAdmin.staticCall(ROLE)).to.equal(
+          newAdminRole,
+        );
+      });
+
+      it('emits RoleAdminChanged event', async () => {
+        const newAdminRole = ethers.solidityPackedKeccak256(
+          ['string'],
+          ['NEW_ADMIN_ROLE'],
+        );
+
+        await expect(instance.$_setRoleAdmin(ROLE, newAdminRole))
+          .to.emit(instance, 'RoleAdminChanged')
+          .withArgs(ROLE, DEFAULT_ADMIN_ROLE, newAdminRole);
+      });
     });
 
-    it('emits RoleAdminChanged event', async () => {
-      const newAdminRole = ethers.solidityPackedKeccak256(
-        ['string'],
-        ['NEW_ADMIN_ROLE'],
-      );
-
-      await expect(instance.$_setRoleAdmin(ROLE, newAdminRole))
-        .to.emit(instance, 'RoleAdminChanged')
-        .withArgs(ROLE, DEFAULT_ADMIN_ROLE, newAdminRole);
-    });
-  });
-
-  describe('#_getRoleMember(bytes32,uint256)', () => {
-    it('returns the correct member', async () => {
-      await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
-      await instance.connect(admin).grantRole(ROLE, nonAdmin2.address);
-      await instance.connect(admin).grantRole(ROLE, nonAdmin3.address);
-
-      expect(await instance.getRoleMember(ROLE, 0)).to.equal(nonAdmin.address);
-      expect(await instance.getRoleMember(ROLE, 1)).to.equal(nonAdmin2.address);
-      expect(await instance.getRoleMember(ROLE, 2)).to.equal(nonAdmin3.address);
-    });
-
-    describe('reverts if', () => {
-      it('role does not exist', async () => {
+    describe('#_getRoleMember(bytes32,uint256)', () => {
+      it('returns the correct member', async () => {
         await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
+        await instance.connect(admin).grantRole(ROLE, nonAdmin2.address);
+        await instance.connect(admin).grantRole(ROLE, nonAdmin3.address);
+
+        expect(await instance.getRoleMember(ROLE, 0)).to.equal(
+          nonAdmin.address,
+        );
+        expect(await instance.getRoleMember(ROLE, 1)).to.equal(
+          nonAdmin2.address,
+        );
+        expect(await instance.getRoleMember(ROLE, 2)).to.equal(
+          nonAdmin3.address,
+        );
+      });
+
+      describe('reverts if', () => {
+        it('role does not exist', async () => {
+          await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
+
+          const newRole = ethers.solidityPackedKeccak256(
+            ['string'],
+            ['NEW_ROLE'],
+          );
+          await expect(
+            instance.getRoleMember(newRole, 0),
+          ).to.revertedWithCustomError(
+            instance,
+            'EnumerableSet__IndexOutOfBounds',
+          );
+        });
+
+        it('role exists but index is invalid', async () => {
+          await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
+
+          await expect(
+            instance.getRoleMember(ROLE, 1),
+          ).to.revertedWithCustomError(
+            instance,
+            'EnumerableSet__IndexOutOfBounds',
+          );
+        });
+      });
+    });
+
+    describe('#_getRoleMemberCount(bytes32)', () => {
+      it('returns the correct count', async () => {
+        await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
+        await instance.connect(admin).grantRole(ROLE, nonAdmin2.address);
+        await instance.connect(admin).grantRole(ROLE, nonAdmin3.address);
+
+        expect(await instance.getRoleMemberCount(ROLE)).to.equal(3);
 
         const newRole = ethers.solidityPackedKeccak256(
           ['string'],
           ['NEW_ROLE'],
         );
-        await expect(
-          instance.getRoleMember(newRole, 0),
-        ).to.revertedWithCustomError(
-          instance,
-          'EnumerableSet__IndexOutOfBounds',
-        );
+        expect(await instance.getRoleMemberCount(newRole)).to.equal(0);
       });
-
-      it('role exists but index is invalid', async () => {
-        await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
-
-        await expect(
-          instance.getRoleMember(ROLE, 1),
-        ).to.revertedWithCustomError(
-          instance,
-          'EnumerableSet__IndexOutOfBounds',
-        );
-      });
-    });
-  });
-
-  describe('#_getRoleMemberCount(bytes32)', () => {
-    it('returns the correct count', async () => {
-      await instance.connect(admin).grantRole(ROLE, nonAdmin.address);
-      await instance.connect(admin).grantRole(ROLE, nonAdmin2.address);
-      await instance.connect(admin).grantRole(ROLE, nonAdmin3.address);
-
-      expect(await instance.getRoleMemberCount(ROLE)).to.equal(3);
-
-      const newRole = ethers.solidityPackedKeccak256(['string'], ['NEW_ROLE']);
-      expect(await instance.getRoleMemberCount(newRole)).to.equal(0);
     });
   });
 });
