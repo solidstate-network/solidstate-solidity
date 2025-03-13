39097d3fb3365e69fd9fd86994a6d21d05ad3967
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-13 15:36:27 -0600

    merge ERC1271Stored into ERC1271Base

--- contracts/signature/ERC1271/base/_ERC1271Base.sol
+++ contracts/signature/ERC1271/base/_ERC1271Base.sol
@@ -3,11 +3,11 @@
 pragma solidity ^0.8.20;
 
 import { IERC1271 } from '../../../interfaces/IERC1271.sol';
+import { ERC1271StoredStorage } from './ERC1271StoredStorage.sol';
 import { _IERC1271Base } from './_IERC1271Base.sol';
 
 /**
  * @title ERC1271Base internal functions
- * @dev _isValidSignature function must be overridden with application-specific logic
  */
 abstract contract _ERC1271Base is _IERC1271Base {
     bytes4 internal constant MAGIC_VALUE = IERC1271.isValidSignature.selector;
@@ -22,6 +22,17 @@ abstract contract _ERC1271Base is _IERC1271Base {
         bytes32 hash,
         bytes memory signature
     ) internal view virtual returns (bytes4 magicValue) {
-        return bytes4(0);
+        return
+            ERC1271StoredStorage
+                .layout(ERC1271StoredStorage.DEFAULT_STORAGE_SLOT)
+                .hashes[hash]
+                ? MAGIC_VALUE
+                : bytes4(0);
+    }
+
+    function _setValidSignature(bytes32 hash, bool status) internal {
+        ERC1271StoredStorage
+            .layout(ERC1271StoredStorage.DEFAULT_STORAGE_SLOT)
+            .hashes[hash] = status;
     }
 }
