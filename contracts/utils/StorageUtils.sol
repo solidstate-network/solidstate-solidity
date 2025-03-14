9b2963916431b68a781a8dbd48eff6d06ccd0ab9
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-14 17:26:17 -0600

    move ERC7201#calculateStorageSlot to StorageUtils

--- contracts/utils/StorageUtils.sol
+++ contracts/utils/StorageUtils.sol
@@ -3,6 +3,20 @@
 pragma solidity ^0.8.20;
 
 library StorageUtils {
+    /**
+     * @notice calculate the EIP-7201 storage slot for a given string id
+     * @dev id parameter should not contain whitespace
+     * @dev see https://eips.ethereum.org/EIPS/eip-7201
+     * @param id namespace id
+     */
+    function calculateStorageSlot(
+        string memory id
+    ) internal pure returns (bytes32 slot) {
+        slot =
+            keccak256(abi.encode(uint256(keccak256(bytes(id))) - 1)) &
+            ~bytes32(uint256(0xff));
+    }
+
     function read(uint256 slot) internal view returns (bytes32 data) {
         assembly {
             data := sload(slot)
