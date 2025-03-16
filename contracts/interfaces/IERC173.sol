6e031dca5351d6ac6ed1da2a8c488fd3f710ae2f
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-15 23:57:49 -0600

    add IERC5313 interfaces

--- contracts/interfaces/IERC173.sol
+++ contracts/interfaces/IERC173.sol
@@ -3,18 +3,13 @@
 pragma solidity ^0.8.20;
 
 import { _IERC173 } from './_IERC173.sol';
+import { IERC5313 } from './IERC5313.sol';
 
 /**
  * @title Contract ownership standard interface
  * @dev see https://eips.ethereum.org/EIPS/eip-173
  */
-interface IERC173 is _IERC173 {
-    /**
-     * @notice get the ERC173 contract owner
-     * @return contract owner
-     */
-    function owner() external view returns (address);
-
+interface IERC173 is _IERC173, IERC5313 {
     /**
      * @notice transfer contract ownership to new account
      * @param account address of new owner
