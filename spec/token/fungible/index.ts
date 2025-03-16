07eca85b31fb05a18d5a7ecd8aecbb2e82e0ad9a
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-15 20:18:39 -0600

    remove FungibleTokenImplicitApproval

--- spec/token/fungible/index.ts
+++ spec/token/fungible/index.ts
@@ -1,6 +1,5 @@
 export * from './FungibleTokenBase.behavior';
 export * from './FungibleTokenExtended.behavior';
-export * from './FungibleTokenImplicitApproval.behavior';
 export * from './FungibleTokenMetadata.behavior';
 export * from './FungibleTokenPermit.behavior';
 export * from './FungibleVaultToken.behavior';
