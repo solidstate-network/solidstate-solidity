e0f50a4f93e44f143541f59350782d4f0aa9f07c
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-17 12:11:25 -0600

    move ERC1155 URI event to _IERC1155Metadata

--- contracts/interfaces/_IERC1155Metadata.sol
+++ contracts/interfaces/_IERC1155Metadata.sol
@@ -2,4 +2,6 @@
 
 pragma solidity ^0.8.20;
 
-interface _IERC1155Metadata {}
+interface _IERC1155Metadata {
+    event URI(string value, uint256 indexed tokenId);
+}
--- contracts/token/multi/metadata/_IMultiTokenMetadata.sol
+++ contracts/token/multi/metadata/_IMultiTokenMetadata.sol
@@ -7,6 +7,4 @@ import { _IERC1155Metadata } from '../../../interfaces/_IERC1155Metadata.sol';
 /**
  * @title Partial MultiTokenMetadata interface needed by internal functions
  */
-interface _IMultiTokenMetadata is _IERC1155Metadata {
-    event URI(string value, uint256 indexed tokenId);
-}
+interface _IMultiTokenMetadata is _IERC1155Metadata {}
