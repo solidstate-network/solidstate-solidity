71b83c8c4d3779e7092af949f31edb8b78a0ee2b
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-17 02:17:04 -0600

    add ERC721Metadata standard interface

--- contracts/token/non_fungible/metadata/INonFungibleTokenMetadata.sol
+++ contracts/token/non_fungible/metadata/INonFungibleTokenMetadata.sol
@@ -2,27 +2,13 @@
 
 pragma solidity ^0.8.20;
 
+import { IERC721Metadata } from '../../../interfaces/IERC721Metadata.sol';
 import { _INonFungibleTokenMetadata } from './_INonFungibleTokenMetadata.sol';
 
 /**
  * @title NonFungibleTokenMetadata interface
  */
-interface INonFungibleTokenMetadata is _INonFungibleTokenMetadata {
-    /**
-     * @notice get token name
-     * @return token name
-     */
-    function name() external view returns (string memory);
-
-    /**
-     * @notice get token symbol
-     * @return token symbol
-     */
-    function symbol() external view returns (string memory);
-
-    /**
-     * @notice get generated URI for given token
-     * @return token URI
-     */
-    function tokenURI(uint256 tokenId) external view returns (string memory);
-}
+interface INonFungibleTokenMetadata is
+    _INonFungibleTokenMetadata,
+    IERC721Metadata
+{}
