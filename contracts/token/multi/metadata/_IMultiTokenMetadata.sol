33fd30255418bbd3bdf0ef09c3d10e23edba5287
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-17 02:56:57 -0600

    add ERC1155Metadata standard interface

--- contracts/token/multi/metadata/_IMultiTokenMetadata.sol
+++ contracts/token/multi/metadata/_IMultiTokenMetadata.sol
@@ -2,9 +2,11 @@
 
 pragma solidity ^0.8.20;
 
+import { _IERC1155Metadata } from '../../../interfaces/_IERC1155Metadata.sol';
+
 /**
  * @title Partial MultiTokenMetadata interface needed by internal functions
  */
-interface _IMultiTokenMetadata {
+interface _IMultiTokenMetadata is _IERC1155Metadata {
     event URI(string value, uint256 indexed tokenId);
 }
