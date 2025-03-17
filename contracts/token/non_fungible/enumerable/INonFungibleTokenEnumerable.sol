7064177cc38fe698e685657dc8406c395f8ba406
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-17 02:21:43 -0600

    add ERC721Enumerable standard interface

--- contracts/token/non_fungible/enumerable/INonFungibleTokenEnumerable.sol
+++ contracts/token/non_fungible/enumerable/INonFungibleTokenEnumerable.sol
@@ -2,36 +2,12 @@
 
 pragma solidity ^0.8.20;
 
+import { IERC721Enumerable } from '../../../interfaces/IERC721Enumerable.sol';
 import { INonFungibleToken } from '../INonFungibleToken.sol';
 import { _INonFungibleTokenEnumerable } from './_NonFungibleTokenEnumerable.sol';
 
 interface INonFungibleTokenEnumerable is
     _INonFungibleTokenEnumerable,
-    INonFungibleToken
-{
-    /**
-     * @notice get total token supply
-     * @return total supply
-     */
-    function totalSupply() external view returns (uint256);
-
-    /**
-     * @notice get token of given owner at given internal storage index
-     * @param owner token holder to query
-     * @param index position in owner's token list to query
-     * @return tokenId id of retrieved token
-     */
-    function tokenOfOwnerByIndex(
-        address owner,
-        uint256 index
-    ) external view returns (uint256 tokenId);
-
-    /**
-     * @notice get token at given internal storage index
-     * @param index position in global token list to query
-     * @return tokenId id of retrieved token
-     */
-    function tokenByIndex(
-        uint256 index
-    ) external view returns (uint256 tokenId);
-}
+    INonFungibleToken,
+    IERC721Enumerable
+{}
