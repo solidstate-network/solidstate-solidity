93d35e2a21bfbc63b866f9b2642f922b35df5798
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-18 13:40:57 -0600

    remove _getRelayedCallSender helper

--- contracts/utils/_MetaTransactionContext.sol
+++ contracts/utils/_MetaTransactionContext.sol
@@ -21,38 +21,29 @@ abstract contract _MetaTransactionContext is _Context {
         returns (address msgSender)
     {
         if (msg.sender == address(this)) {
-            msgSender = _getRelayedCallSender();
+            // We need to read 20 bytes (an address) located at array index msg.data.length - 20. In memory, the array
+            // is prefixed with a 32-byte length value, so we first add 32 to get the memory read index. However, doing
+            // so would leave the address in the upper 20 bytes of the 32-byte word, which is inconvenient and would
+            // require bit shifting. We therefore subtract 12 from the read index so the address lands on the lower 20
+            // bytes. This can always be done due to the 32-byte prefix.
+
+            // The final memory read index is msg.data.length - 20 + 32 - 12 = msg.data.length. Using inline assembly is the
+            // easiest/most-efficient way to perform this operation.
+
+            // These fields are not accessible from assembly
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+
+            // solhint-disable-next-line no-inline-assembly
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                msgSender := and(
+                    mload(add(array, index)),
+                    0xffffffffffffffffffffffffffffffffffffffff
+                )
+            }
         } else {
             msgSender = msg.sender;
         }
-    }
-
-    function _getRelayedCallSender()
-        private
-        pure
-        returns (address payable result)
-    {
-        // We need to read 20 bytes (an address) located at array index msg.data.length - 20. In memory, the array
-        // is prefixed with a 32-byte length value, so we first add 32 to get the memory read index. However, doing
-        // so would leave the address in the upper 20 bytes of the 32-byte word, which is inconvenient and would
-        // require bit shifting. We therefore subtract 12 from the read index so the address lands on the lower 20
-        // bytes. This can always be done due to the 32-byte prefix.
-
-        // The final memory read index is msg.data.length - 20 + 32 - 12 = msg.data.length. Using inline assembly is the
-        // easiest/most-efficient way to perform this operation.
-
-        // These fields are not accessible from assembly
-        bytes memory array = msg.data;
-        uint256 index = msg.data.length;
-
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
-            result := and(
-                mload(add(array, index)),
-                0xffffffffffffffffffffffffffffffffffffffff
-            )
-        }
-        return result;
     }
 }
