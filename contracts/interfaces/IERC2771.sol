d71154c35680bf6a73c9b1013562dc404e0bb532
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-18 14:25:32 -0600

    add comments to IERC2771

--- contracts/interfaces/IERC2771.sol
+++ contracts/interfaces/IERC2771.sol
@@ -4,6 +4,17 @@ pragma solidity ^0.8.20;
 
 import { _IERC2771 } from './_IERC2771.sol';
 
+/**
+ * @title ERC2771 interface
+ * @dev see https://eips.ethereum.org/EIPS/eip-2771
+ */
 interface IERC2771 is _IERC2771 {
-    function isTrustedForwarder(address forwarder) external view returns (bool);
+    /**
+     * @notice query whether account is a trusted ERC2771 forwarder
+     * @param account address to query
+     * @return trustedStatus whether account is a trusted forwarder
+     */
+    function isTrustedForwarder(
+        address account
+    ) external view returns (bool trustedStatus);
 }
--- contracts/utils/ForwardedMetaTransationContext.sol
+++ contracts/utils/ForwardedMetaTransationContext.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.20;
 
+import { IERC2771 } from '../interfaces/IERC2771.sol';
 import { IForwardedMetaTransationContext } from './IForwardedMetaTransationContext.sol';
 import { _ForwardedMetaTransationContext } from './_ForwardedMetaTransationContext.sol';
 
@@ -9,6 +10,9 @@ abstract contract ForwardedMetaTransationContext is
     IForwardedMetaTransationContext,
     _ForwardedMetaTransationContext
 {
+    /**
+     * @inheritdoc IERC2771
+     */
     function isTrustedForwarder(
         address account
     ) external view returns (bool trustedStatus) {
