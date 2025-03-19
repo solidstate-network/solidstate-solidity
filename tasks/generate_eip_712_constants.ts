f6ff2066b960839bb8428dc2cb35fd00458b41a9
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-19 14:27:01 -0600

    generate domain separator calculation functions

--- tasks/generate_eip_712_constants.ts
+++ tasks/generate_eip_712_constants.ts
@@ -8,29 +8,81 @@ const filepath = 'cryptography';
 task('generate-eip-712-constants', `Generate ${filename}`).setAction(
   async (args, hre) => {
     const domainFields = [
-      'string name',
-      'string version',
-      'uint256 chainId',
-      'address verifyingContract',
-      'bytes32 salt',
-    ];
+      'name',
+      'version',
+      'chainId',
+      'verifyingContract',
+      'salt',
+    ] as const;
+
+    const domainFieldParameterMap = {
+      name: 'string name',
+      version: 'string version',
+      chainId: 'uint256 chainId',
+      verifyingContract: 'address verifyingContract',
+      salt: 'bytes32 salt',
+    };
+
+    // TODO: remove chainId and verifyingContract
+    const domainSeparatorCalculatorParameterMap = {
+      name: 'bytes32 nameHash',
+      version: 'bytes32 versionHash',
+      chainId: 'uint256 chainId',
+      verifyingContract: 'address verifyingContract',
+      salt: 'bytes32 salt',
+    };
 
     const domainHashes = [];
+    const domainSeparatorCalculators = [];
 
     for (let i = 0; i < 2 ** domainFields.length; i++) {
       const fieldsBinString = i.toString(2).padStart(5, '0');
+      const includedFields = domainFields.filter((f, j) => i & (2 ** j));
 
-      const components = domainFields.filter((f, j) => i & (2 ** j));
+      const domainStringComponents = includedFields.map(
+        (f) => domainFieldParameterMap[f],
+      );
 
-      const domainString = `EIP712Domain(${components.join(',')})`;
+      const domainSeparatorCalculatorComponents = includedFields.map(
+        (c) => domainSeparatorCalculatorParameterMap[c],
+      );
+      const assemblyComponents = domainSeparatorCalculatorComponents.map(
+        (c, j) =>
+          `mstore(add(pointer, ${(j + 1) * 32}), ${c.split(' ').pop()})`,
+      );
+
+      const domainString = `EIP712Domain(${domainStringComponents.join(',')})`;
+      const keccak = hre.ethers.solidityPackedKeccak256(
+        ['string'],
+        [domainString],
+      );
 
       domainHashes.push(
         `
         /**
-         * @dev EIP712Domain hash corresponding to ERC5267 fields value ${fieldsBinString} (${components.map((c) => c.split(' ').pop()).join(', ')})
-         * @dev evaluates to ${hre.ethers.solidityPackedKeccak256(['string'], [domainString])}
+         * @dev EIP712Domain hash corresponding to ERC5267 fields value ${fieldsBinString} (${domainStringComponents.map((c) => c.split(' ').pop()).join(', ')})
+         * @dev evaluates to ${keccak}
          */
         bytes32 internal constant EIP_712_DOMAIN_HASH_${fieldsBinString} = keccak256('${domainString}');
+        `,
+      );
+
+      domainSeparatorCalculators.push(
+        `
+        /**
+         * @notice TODO
+         */
+        function calculateDomainSeparator_${fieldsBinString}(${domainSeparatorCalculatorComponents.join(', ')}) internal view returns (bytes32 domainSeparator) {
+          assembly {
+            let pointer := mload(64)
+
+            mstore(pointer, ${keccak})
+            ${assemblyComponents.join('\n')}
+
+            domainSeparator := keccak256(pointer, ${(assemblyComponents.length + 1) * 32})
+
+          }
+        }
         `,
       );
     }
@@ -40,6 +92,8 @@ task('generate-eip-712-constants', `Generate ${filename}`).setAction(
 
       library ${filename.split('.').shift()} {
         ${domainHashes.join('\n')}
+
+        ${domainSeparatorCalculators.join('\n')}
       }
 `;
 
