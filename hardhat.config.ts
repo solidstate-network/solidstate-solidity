26137acd39e2270e216b539826b8e301549d3ad0
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-20 13:00:04 -0600

    set evmVerion to cancun

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,6 +18,7 @@ const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.28',
     settings: {
+      evmVersion: 'cancun',
       optimizer: {
         enabled: true,
         runs: 200,
