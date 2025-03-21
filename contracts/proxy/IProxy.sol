76de1431ba3cbd8238662f0c816b21d6b9450b8b
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-21 16:15:19 -0600

    move comment to interface

--- contracts/proxy/IProxy.sol
+++ contracts/proxy/IProxy.sol
@@ -6,5 +6,8 @@ import { IERC1967Proxy } from '../interfaces/IERC1967Proxy.sol';
 import { _IProxy } from './_IProxy.sol';
 
 interface IProxy is _IProxy, IERC1967Proxy {
+    /**
+     * @notice delegate calls to EIP-1967 implementation contract
+     */
     fallback() external payable;
 }
