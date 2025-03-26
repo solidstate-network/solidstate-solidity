1b524424364363480fc807f6ff27ac0117a6395f
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-25 18:51:29 -0600

    use StorageUtils slot accessors for TransientReentrancyGuard

--- contracts/security/reentrancy_guard/_TransientReentrancyGuard.sol
+++ contracts/security/reentrancy_guard/_TransientReentrancyGuard.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.20;
 
 import { ReentrancyGuardStorage } from '../../storage/ReentrancyGuardStorage.sol';
+import { Bytes32Utils } from '../../utils/Bytes32Utils.sol';
+import { StorageUtils } from '../../utils/StorageUtils.sol';
 import { _ReentrancyGuard } from './_ReentrancyGuard.sol';
 import { _ITransientReentrancyGuard } from './_ITransientReentrancyGuard.sol';
 
@@ -10,6 +12,9 @@ abstract contract _TransientReentrancyGuard is
     _ITransientReentrancyGuard,
     _ReentrancyGuard
 {
+    using Bytes32Utils for bytes32;
+    using StorageUtils for bytes32;
+
     /**
      * @notice returns true if the reentrancy guard is locked, false otherwise
      * @return status whether guard is locked
@@ -21,32 +26,23 @@ abstract contract _TransientReentrancyGuard is
         override
         returns (bool status)
     {
-        bytes32 slot = ReentrancyGuardStorage.DEFAULT_STORAGE_SLOT;
-
-        assembly {
-            status := tload(slot)
-        }
+        status = ReentrancyGuardStorage
+            .DEFAULT_STORAGE_SLOT
+            .readTransient()
+            .toBool();
     }
 
     /**
      * @notice lock functions that use the nonReentrant modifier
      */
     function _lockReentrancyGuard() internal virtual override {
-        bytes32 slot = ReentrancyGuardStorage.DEFAULT_STORAGE_SLOT;
-
-        assembly {
-            tstore(slot, true)
-        }
+        ReentrancyGuardStorage.DEFAULT_STORAGE_SLOT.writeTransient(true);
     }
 
     /**
      * @notice unlock functions that use the nonReentrant modifier
      */
     function _unlockReentrancyGuard() internal virtual override {
-        bytes32 slot = ReentrancyGuardStorage.DEFAULT_STORAGE_SLOT;
-
-        assembly {
-            tstore(slot, false)
-        }
+        ReentrancyGuardStorage.DEFAULT_STORAGE_SLOT.writeTransient(false);
     }
 }
