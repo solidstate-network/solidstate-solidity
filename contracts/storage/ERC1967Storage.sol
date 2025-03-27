5222de3ec4f2d47db7d36188d344276c3c7072a2
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-27 15:06:42 -0600

    use Slot.StorageSlot for ERC1967Storage library

--- contracts/storage/ERC1967Storage.sol
+++ contracts/storage/ERC1967Storage.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.24;
 
+import { Slot } from '../data/Slot.sol';
+
 library ERC1967Storage {
     /**
      * @custom:storage-location erc7201:solidstate.layout.ERC1967
@@ -36,25 +38,35 @@ library ERC1967Storage {
         address admin;
     }
 
-    bytes32 internal constant IMPLEMENTATION_STORAGE_SLOT =
-        bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1);
-    bytes32 internal constant BEACON_STORAGE_SLOT =
-        bytes32(uint256(keccak256('eip1967.proxy.beacon')) - 1);
-    bytes32 internal constant ADMIN_STORAGE_SLOT =
-        bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1);
+    Slot.StorageSlot internal constant IMPLEMENTATION_STORAGE_SLOT =
+        Slot.StorageSlot.wrap(
+            bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)
+        );
+    Slot.StorageSlot internal constant BEACON_STORAGE_SLOT =
+        Slot.StorageSlot.wrap(
+            bytes32(uint256(keccak256('eip1967.proxy.beacon')) - 1)
+        );
+    Slot.StorageSlot internal constant ADMIN_STORAGE_SLOT =
+        Slot.StorageSlot.wrap(
+            bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1)
+        );
 
-    bytes32 internal constant DEFAULT_STORAGE_SLOT =
-        keccak256(
-            abi.encode(
-                uint256(keccak256(bytes('solidstate.layout.ERC1967'))) - 1
-            )
-        ) & ~bytes32(uint256(0xff));
+    Slot.StorageSlot internal constant DEFAULT_STORAGE_SLOT =
+        Slot.StorageSlot.wrap(
+            keccak256(
+                abi.encode(
+                    uint256(keccak256(bytes('solidstate.layout.ERC1967'))) - 1
+                )
+            ) & ~bytes32(uint256(0xff))
+        );
 
     function layout() internal pure returns (Layout storage $) {
         $ = layout(DEFAULT_STORAGE_SLOT);
     }
 
-    function layout(bytes32 slot) internal pure returns (Layout storage $) {
+    function layout(
+        Slot.StorageSlot slot
+    ) internal pure returns (Layout storage $) {
         assembly {
             $.slot := slot
         }
@@ -65,7 +77,7 @@ library ERC1967Storage {
         pure
         returns (ImplementationLayout storage $)
     {
-        bytes32 slot = IMPLEMENTATION_STORAGE_SLOT;
+        Slot.StorageSlot slot = IMPLEMENTATION_STORAGE_SLOT;
 
         assembly {
             $.slot := slot
@@ -73,7 +85,7 @@ library ERC1967Storage {
     }
 
     function beaconLayout() internal pure returns (BeaconLayout storage $) {
-        bytes32 slot = BEACON_STORAGE_SLOT;
+        Slot.StorageSlot slot = BEACON_STORAGE_SLOT;
 
         assembly {
             $.slot := slot
@@ -81,7 +93,7 @@ library ERC1967Storage {
     }
 
     function adminLayout() internal pure returns (AdminLayout storage $) {
-        bytes32 slot = ADMIN_STORAGE_SLOT;
+        Slot.StorageSlot slot = ADMIN_STORAGE_SLOT;
 
         assembly {
             $.slot := slot
