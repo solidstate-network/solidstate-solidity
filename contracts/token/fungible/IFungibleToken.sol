93dd708c7766436698c1311da552b076f5e0080b
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-28 21:51:13 -0600

    add missing imports of external Context contract and interface

--- contracts/token/fungible/IFungibleToken.sol
+++ contracts/token/fungible/IFungibleToken.sol
@@ -3,9 +3,10 @@
 pragma solidity ^0.8.24;
 
 import { IERC20 } from '../../interfaces/IERC20.sol';
+import { IContext } from '../../meta/IContext.sol';
 import { _IFungibleToken } from './_IFungibleToken.sol';
 
 /**
  * @title FungibleToken base interface
  */
-interface IFungibleToken is _IFungibleToken, IERC20 {}
+interface IFungibleToken is _IFungibleToken, IERC20, IContext {}
--- contracts/token/multi/IMultiToken.sol
+++ contracts/token/multi/IMultiToken.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.24;
 
 import { IERC1155 } from '../../interfaces/IERC1155.sol';
 import { IIntrospectable } from '../../introspection/IIntrospectable.sol';
+import { IContext } from '../../meta/IContext.sol';
 import { _IMultiToken } from './_IMultiToken.sol';
 
 /**
  * @title MultiToken base interface
  */
-interface IMultiToken is _IMultiToken, IERC1155, IIntrospectable {}
+interface IMultiToken is _IMultiToken, IERC1155, IIntrospectable, IContext {}
