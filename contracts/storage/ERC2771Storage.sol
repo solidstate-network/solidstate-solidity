c8e25fe3ad55cdd08ae4e49251204603ea1fe976
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-28 22:05:56 -0600

    use Slot for ERC2771Storage library

--- contracts/storage/ERC2771Storage.sol
+++ contracts/storage/ERC2771Storage.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.24;
 
+import { Slot } from '../data/Slot.sol';
+
 library ERC2771Storage {
     /**
      * @custom:storage-location erc7201:solidstate.layout.ERC2771
@@ -10,18 +12,22 @@ library ERC2771Storage {
         mapping(address account => bool trustedStatus) trustedForwarders;
     }
 
-    bytes32 internal constant DEFAULT_STORAGE_SLOT =
-        keccak256(
-            abi.encode(
-                uint256(keccak256(bytes('solidstate.layout.ERC2771'))) - 1
-            )
-        ) & ~bytes32(uint256(0xff));
+    Slot.StorageSlot internal constant DEFAULT_STORAGE_SLOT =
+        Slot.StorageSlot.wrap(
+            keccak256(
+                abi.encode(
+                    uint256(keccak256(bytes('solidstate.layout.ERC2771'))) - 1
+                )
+            ) & ~bytes32(uint256(0xff))
+        );
 
     function layout() internal pure returns (Layout storage $) {
         $ = layout(DEFAULT_STORAGE_SLOT);
     }
 
-    function layout(bytes32 slot) internal pure returns (Layout storage $) {
+    function layout(
+        Slot.StorageSlot slot
+    ) internal pure returns (Layout storage $) {
         assembly {
             $.slot := slot
         }
