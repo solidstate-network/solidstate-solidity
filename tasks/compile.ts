03df20200c87c401126250f73fb4606d645b051a
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-04 22:00:42 -0600

    delete Solidity barrel file before compilation

--- tasks/compile.ts
+++ tasks/compile.ts
@@ -1,7 +1,12 @@
+import fs from 'fs';
 import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
 import { task } from 'hardhat/config';
+import path from 'path';
 
 task(TASK_COMPILE).setAction(async (args, hre, runSuper) => {
+  await fs.promises.rm(path.resolve(hre.config.paths.sources, 'index.sol'), {
+    force: true,
+  });
   await runSuper(args);
   await hre.run('organize-test-files');
 });
