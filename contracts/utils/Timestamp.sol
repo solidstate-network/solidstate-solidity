3af729ce0dc94ab8af09dcdf28f8e4b095bca528
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-05 16:11:30 -0600

    add Timestamp type with free comparator functions

---
+++ contracts/utils/Timestamp.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+type Timestamp is uint48;
+
+using { eq as ==, gt as >, lt as <, gte as >=, lte as <= } for Timestamp global;
+
+function eq(Timestamp t0, Timestamp t1) pure returns (bool status) {
+    status = Timestamp.unwrap(t0) == Timestamp.unwrap(t1);
+}
+
+function gt(Timestamp t0, Timestamp t1) pure returns (bool status) {
+    status = Timestamp.unwrap(t0) > Timestamp.unwrap(t1);
+}
+
+function lt(Timestamp t0, Timestamp t1) pure returns (bool status) {
+    status = Timestamp.unwrap(t0) < Timestamp.unwrap(t1);
+}
+
+function gte(Timestamp t0, Timestamp t1) pure returns (bool status) {
+    status = Timestamp.unwrap(t0) >= Timestamp.unwrap(t1);
+}
+
+function lte(Timestamp t0, Timestamp t1) pure returns (bool status) {
+    status = Timestamp.unwrap(t0) <= Timestamp.unwrap(t1);
+}
