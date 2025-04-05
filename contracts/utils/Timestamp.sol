3d7e639ceb19cb9b46bee5700109b578347aa069
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-05 16:13:22 -0600

    add != Timestamp operator

--- contracts/utils/Timestamp.sol
+++ contracts/utils/Timestamp.sol
@@ -4,10 +4,21 @@ pragma solidity ^0.8.24;
 
 type Timestamp is uint48;
 
-using { eq as ==, gt as >, lt as <, gte as >=, lte as <= } for Timestamp global;
+using {
+    eq as ==,
+    notEq as !=,
+    gt as >,
+    lt as <,
+    gte as >=,
+    lte as <=
+} for Timestamp global;
 
 function eq(Timestamp t0, Timestamp t1) pure returns (bool status) {
     status = Timestamp.unwrap(t0) == Timestamp.unwrap(t1);
+}
+
+function notEq(Timestamp t0, Timestamp t1) pure returns (bool status) {
+    status = Timestamp.unwrap(t0) != Timestamp.unwrap(t1);
 }
 
 function gt(Timestamp t0, Timestamp t1) pure returns (bool status) {
