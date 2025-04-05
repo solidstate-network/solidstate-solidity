083f969f3b6d8fd41328ef7285378271c7067ddf
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-05 17:37:23 -0600

    fix Duration variable names

--- contracts/utils/Duration.sol
+++ contracts/utils/Duration.sol
@@ -16,34 +16,34 @@ using {
     sub as -
 } for Duration global;
 
-function eq(Duration t0, Duration t1) pure returns (bool status) {
-    status = Duration.unwrap(t0) == Duration.unwrap(t1);
+function eq(Duration d0, Duration d1) pure returns (bool status) {
+    status = Duration.unwrap(d0) == Duration.unwrap(d1);
 }
 
-function notEq(Duration t0, Duration t1) pure returns (bool status) {
-    status = Duration.unwrap(t0) != Duration.unwrap(t1);
+function notEq(Duration d0, Duration d1) pure returns (bool status) {
+    status = Duration.unwrap(d0) != Duration.unwrap(d1);
 }
 
-function gt(Duration t0, Duration t1) pure returns (bool status) {
-    status = Duration.unwrap(t0) > Duration.unwrap(t1);
+function gt(Duration d0, Duration d1) pure returns (bool status) {
+    status = Duration.unwrap(d0) > Duration.unwrap(d1);
 }
 
-function lt(Duration t0, Duration t1) pure returns (bool status) {
-    status = Duration.unwrap(t0) < Duration.unwrap(t1);
+function lt(Duration d0, Duration d1) pure returns (bool status) {
+    status = Duration.unwrap(d0) < Duration.unwrap(d1);
 }
 
-function gte(Duration t0, Duration t1) pure returns (bool status) {
-    status = Duration.unwrap(t0) >= Duration.unwrap(t1);
+function gte(Duration d0, Duration d1) pure returns (bool status) {
+    status = Duration.unwrap(d0) >= Duration.unwrap(d1);
 }
 
-function lte(Duration t0, Duration t1) pure returns (bool status) {
-    status = Duration.unwrap(t0) <= Duration.unwrap(t1);
+function lte(Duration d0, Duration d1) pure returns (bool status) {
+    status = Duration.unwrap(d0) <= Duration.unwrap(d1);
 }
 
-function add(Duration t0, Duration t1) pure returns (Duration duration) {
-    duration = Duration.wrap(Duration.unwrap(t0) + Duration.unwrap(t1));
+function add(Duration d0, Duration d1) pure returns (Duration duration) {
+    duration = Duration.wrap(Duration.unwrap(d0) + Duration.unwrap(d1));
 }
 
-function sub(Duration t0, Duration t1) pure returns (Duration duration) {
-    duration = Duration.wrap(Duration.unwrap(t0) - Duration.unwrap(t1));
+function sub(Duration d0, Duration d1) pure returns (Duration duration) {
+    duration = Duration.wrap(Duration.unwrap(d0) - Duration.unwrap(d1));
 }
