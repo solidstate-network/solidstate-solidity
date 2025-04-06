666488d5e5673e8b5e979c26bd85f7baf6ea6b70
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-05 19:20:39 -0600

    add Duration static factory functions

--- contracts/utils/Duration.sol
+++ contracts/utils/Duration.sol
@@ -56,4 +56,28 @@ library Duration {
     ) internal pure returns (duration result) {
         result = duration.wrap(uint48(value));
     }
+
+    function ofSeconds(
+        uint256 quantity
+    ) internal pure returns (duration result) {
+        result = fromUint256(quantity * 1 seconds);
+    }
+
+    function ofMinutes(
+        uint256 quantity
+    ) internal pure returns (duration result) {
+        result = fromUint256(quantity * 1 minutes);
+    }
+
+    function ofHours(uint256 quantity) internal pure returns (duration result) {
+        result = fromUint256(quantity * 1 hours);
+    }
+
+    function ofDays(uint256 quantity) internal pure returns (duration result) {
+        result = fromUint256(quantity * 1 days);
+    }
+
+    function ofWeeks(uint256 quantity) internal pure returns (duration result) {
+        result = fromUint256(quantity * 1 weeks);
+    }
 }
