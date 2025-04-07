9a35a40e2370450104abdee4b94419b71f546781
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-06 19:25:23 -0600

    remove tests for removed functions

--- test/access/access_control/ownable/AccessControlOwnable.ts
+++ test/access/access_control/ownable/AccessControlOwnable.ts
@@ -14,7 +14,6 @@ describe('AccessControlOwnable', () => {
   let nonAdmin2: SignerWithAddress;
   let nonAdmin3: SignerWithAddress;
   let instance: $AccessControlOwnable;
-  const oneDayInSeconds = 86400;
 
   before(async () => {
     [defaultAdmin, nonAdmin, nonAdmin2, nonAdmin3] = await ethers.getSigners();
@@ -76,128 +75,6 @@ describe('AccessControlOwnable', () => {
             .connect(nonAdmin)
             .grantRole(ethers.id('ROLE'), nonAdmin.address),
         )
-          .to.be.revertedWithCustomError(
-            instance,
-            'AccessControl__Unauthorized',
-          )
-          .withArgs(DEFAULT_ADMIN_ROLE, await nonAdmin.getAddress());
-      });
-    });
-  });
-
-  describe('#beginDefaultAdminTransfer(address,uint256)', () => {
-    it('starts the transfer process', async () => {
-      await instance.beginDefaultAdminTransfer(nonAdmin.address);
-
-      const transfer = await instance.pendingDefaultAdmin();
-      expect(transfer.newAdmin).to.equal(nonAdmin.address);
-      expect(transfer.acceptSchedule).to.be.gt(0); // Ensure end time is set
-    });
-
-    describe('reverts if', () => {
-      it('sender is not default admin', async () => {
-        await expect(
-          instance
-            .connect(nonAdmin)
-            .beginDefaultAdminTransfer(nonAdmin2.address),
-        )
-          .to.be.revertedWithCustomError(
-            instance,
-            'AccessControl__Unauthorized',
-          )
-          .withArgs(DEFAULT_ADMIN_ROLE, await nonAdmin.getAddress());
-      });
-    });
-  });
-
-  describe('#cancelDefaultAdminTransfer()', () => {
-    it('cancels the transfer process', async () => {
-      await instance.beginDefaultAdminTransfer(nonAdmin.address);
-      await instance.cancelDefaultAdminTransfer();
-
-      const transfer = await instance.pendingDefaultAdmin();
-      expect(transfer.newAdmin).to.equal(ethers.ZeroAddress);
-      expect(transfer.acceptSchedule).to.equal(0);
-    });
-
-    describe('reverts if', () => {
-      it('sender is not current or pending default admin', async () => {
-        await instance.beginDefaultAdminTransfer(nonAdmin.address);
-
-        await expect(instance.connect(nonAdmin2).cancelDefaultAdminTransfer())
-          .to.be.revertedWithCustomError(
-            instance,
-            'AccessControl__Unauthorized',
-          )
-          .withArgs(DEFAULT_ADMIN_ROLE, await nonAdmin2.getAddress());
-      });
-    });
-  });
-
-  describe('#acceptDefaultAdminTransfer()', () => {
-    it('completes the transfer process', async () => {
-      await instance.changeDefaultAdminDelay(oneDayInSeconds);
-      await ethers.provider.send('evm_increaseTime', [oneDayInSeconds]);
-      await ethers.provider.send('evm_mine', []);
-      await instance.beginDefaultAdminTransfer(nonAdmin.address);
-      await ethers.provider.send('evm_increaseTime', [oneDayInSeconds]);
-      await ethers.provider.send('evm_mine', []);
-      await instance.connect(nonAdmin).acceptDefaultAdminTransfer();
-
-      expect(await instance.defaultAdmin()).to.equal(nonAdmin.address);
-    });
-
-    describe('reverts if', () => {
-      it('transfer delay has not passed', async () => {
-        await instance.changeDefaultAdminDelay(oneDayInSeconds);
-        await ethers.provider.send('evm_increaseTime', [oneDayInSeconds]);
-        await ethers.provider.send('evm_mine', []);
-
-        await instance.beginDefaultAdminTransfer(nonAdmin.address);
-        await expect(
-          instance.connect(nonAdmin).acceptDefaultAdminTransfer(),
-        ).to.be.revertedWithCustomError(
-          instance,
-          'AccessControlEnforcedDefaultAdminDelay',
-        );
-      });
-    });
-  });
-
-  describe('#changeDefaultAdminDelay(uint256)', () => {
-    it('changes the default admin delay', async () => {
-      const newDelay = oneDayInSeconds;
-      await instance.changeDefaultAdminDelay(newDelay);
-      await ethers.provider.send('evm_increaseTime', [oneDayInSeconds + 1]); // We wait for the newDelay to be set as the new delay
-      await ethers.provider.send('evm_mine', []);
-
-      expect(await instance.defaultAdminDelay()).to.equal(newDelay);
-    });
-
-    describe('reverts if', () => {
-      it('sender is not default admin', async () => {
-        await expect(instance.connect(nonAdmin).changeDefaultAdminDelay(2000))
-          .to.be.revertedWithCustomError(
-            instance,
-            'AccessControl__Unauthorized',
-          )
-          .withArgs(DEFAULT_ADMIN_ROLE, await nonAdmin.getAddress());
-      });
-    });
-  });
-
-  describe('#rollbackDefaultAdminDelay()', () => {
-    it('rolls back the default admin delay to previous value', async () => {
-      const initialDelay = await instance.defaultAdminDelay();
-      await instance.changeDefaultAdminDelay(2000);
-      await instance.rollbackDefaultAdminDelay();
-
-      expect(await instance.defaultAdminDelay()).to.equal(initialDelay);
-    });
-
-    describe('reverts if', () => {
-      it('sender is not default admin', async () => {
-        await expect(instance.connect(nonAdmin).rollbackDefaultAdminDelay())
           .to.be.revertedWithCustomError(
             instance,
             'AccessControl__Unauthorized',
