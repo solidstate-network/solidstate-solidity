7d473238d2d458a29e60c690c84e4e24835e8d30
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-06 19:02:19 -0600

    update pragma version statements

--- contracts/access/access_control/ownable/AccessControlOwnable.sol
+++ contracts/access/access_control/ownable/AccessControlOwnable.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import { Ownable } from '../../ownable/Ownable.sol';
 import { _Ownable } from '../../ownable/_Ownable.sol';
