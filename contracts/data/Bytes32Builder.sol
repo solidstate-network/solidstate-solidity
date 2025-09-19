ff1187763f4edb51a7731b30848dff3867f8672e
Nick Barry <itsnickbarry@protonmail.ch>
2025-09-18 20:16:52 -0600

    add license identifier to Bytes32Builder files

--- contracts/data/Bytes32Builder.sol
+++ contracts/data/Bytes32Builder.sol
@@ -1,4 +1,4 @@
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import { Address } from '../utils/Address.sol';
--- contracts/test/Bytes32BuilderTest.sol
+++ contracts/test/Bytes32BuilderTest.sol
@@ -1,4 +1,4 @@
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import { Bytes32Builder } from '../data/Bytes32Builder.sol';
